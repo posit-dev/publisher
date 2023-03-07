a4e1b1341271247ee9f198485c5972c164e53e82
Michael Marchetti <mike@rstudio.com>
2023-03-07 12:02:07 -0500

    formatting

--- src/connect-client/apitypes/types.go
+++ src/connect-client/apitypes/types.go
@@ -33,43 +33,29 @@ type NullTime struct {
 }
 
 func NewNullString(value string) NullString {
-	return NullString{
-		NewOptional(value),
-	}
+	return NullString{NewOptional(value)}
 }
 
 func NewNullInt32(value int32) NullInt32 {
-	return NullInt32{
-		NewOptional(value),
-	}
+	return NullInt32{NewOptional(value)}
 }
 
 func NewNullInt64(value int64) NullInt64 {
-	return NullInt64{
-		NewOptional(value),
-	}
+	return NullInt64{NewOptional(value)}
 }
 
 func NewNullFloat64(value float64) NullFloat64 {
-	return NullFloat64{
-		NewOptional(value),
-	}
+	return NullFloat64{NewOptional(value)}
 }
 
 func NewNullTime(value Time) NullTime {
-	return NullTime{
-		NewOptional(value),
-	}
+	return NullTime{NewOptional(value)}
 }
 
 func NewNullGUID(value GUID) NullGUID {
-	return NullGUID{
-		NewOptional(value),
-	}
+	return NullGUID{NewOptional(value)}
 }
 
 func NewNullInt64Str(value Int64Str) NullInt64Str {
-	return NullInt64Str{
-		NewOptional(value),
-	}
+	return NullInt64Str{NewOptional(value)}
 }
