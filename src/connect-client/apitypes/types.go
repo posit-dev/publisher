5291cc4138aa339d7ad4ab174f8335a1442a5682
Michael Marchetti <mike@rstudio.com>
2023-03-07 13:27:40 -0500

    simplify optionals

--- src/connect-client/apitypes/types.go
+++ src/connect-client/apitypes/types.go
@@ -6,56 +6,12 @@ import "time"
 
 type GUID string
 type Int64Str string
-type Time struct {
-	time.Time
-}
+type Time = time.Time
 
-type NullString struct {
-	Optional[string]
-}
-type NullInt32 struct {
-	Optional[int32]
-}
-type NullInt64 struct {
-	Optional[int64]
-}
-type NullFloat64 struct {
-	Optional[float64]
-}
-type NullGUID struct {
-	Optional[GUID]
-}
-type NullInt64Str struct {
-	Optional[Int64Str]
-}
-type NullTime struct {
-	Optional[Time]
-}
-
-func NewNullString(value string) NullString {
-	return NullString{NewOptional(value)}
-}
-
-func NewNullInt32(value int32) NullInt32 {
-	return NullInt32{NewOptional(value)}
-}
-
-func NewNullInt64(value int64) NullInt64 {
-	return NullInt64{NewOptional(value)}
-}
-
-func NewNullFloat64(value float64) NullFloat64 {
-	return NullFloat64{NewOptional(value)}
-}
-
-func NewNullTime(value Time) NullTime {
-	return NullTime{NewOptional(value)}
-}
-
-func NewNullGUID(value GUID) NullGUID {
-	return NullGUID{NewOptional(value)}
-}
-
-func NewNullInt64Str(value Int64Str) NullInt64Str {
-	return NullInt64Str{NewOptional(value)}
-}
+type NullString = Optional[string]
+type NullInt32 = Optional[int32]
+type NullInt64 = Optional[int64]
+type NullFloat64 = Optional[float64]
+type NullGUID = Optional[GUID]
+type NullInt64Str = Optional[Int64Str]
+type NullTime = Optional[Time]
