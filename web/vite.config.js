1f85370655423058001690d2a6f51eb02418d201
tdstein <taylor.steinberg@posit.co>
2023-03-08 15:26:37 -0500

    Adds jsconfig support to Vite.

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,4 +1,5 @@
 import eslint from 'vite-plugin-eslint'
+import jsconfigpaths from 'vite-jsconfig-paths'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
@@ -7,6 +8,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [
     eslint(),
+    jsconfigpaths(),
     vue(),
     vuetify()
   ]
