617d3f8cfd02b3a7f648859e2a7feb3e496947bd
Michael Marchetti <mike@rstudio.com>
2023-03-08 15:14:55 -0500

    client simplification

--- src/connect-client/api_client/clients/client.go
+++ src/connect-client/api_client/clients/client.go
@@ -2,9 +2,7 @@ package clients
 
 import (
 	"connect-client/apitypes"
-	"fmt"
 	"io"
-	"time"
 )
 
 // Copyright (C) 2023 by Posit Software, PBC.
@@ -24,21 +22,13 @@ type User struct {
 	Email     string
 }
 
-// Simplified task structure common to all servers
-type Task struct {
-	Finished   bool
-	Output     []string
-	Error      string
-	TotalLines int32
-}
-
 type APIClient interface {
 	TestConnection() error
 	TestAuthentication() (*User, error)
 	CreateDeployment(name ContentName, title apitypes.NullString) (ContentID, error)
 	UploadBundle(ContentID, io.Reader) (BundleID, error)
 	DeployBundle(ContentID, BundleID) (TaskID, error)
-	GetTask(TaskID, *Task) (*Task, error)
+	WaitForTask(taskID TaskID, logWriter io.Writer) error
 }
 
 // PublishingClient provides higher-level client methods that work
@@ -46,27 +36,4 @@ type APIClient interface {
 // any details of specific server types.
 type PublishingClient struct {
 	APIClient
-}
-
-func (client *PublishingClient) WaitForTask(taskID TaskID, logWriter io.Writer) error {
-	var previous *Task
-	for {
-		task, err := client.GetTask(taskID, previous)
-		if err != nil {
-			return err
-		}
-		for _, line := range task.Output {
-			_, err = fmt.Fprintln(logWriter, line)
-			if err != nil {
-				return err
-			}
-		}
-		if task.Finished {
-			if task.Error != "" {
-				return fmt.Errorf("Error from the server: %s", task.Error)
-			}
-			return nil
-		}
-		time.Sleep(1.0)
-	}
 }
