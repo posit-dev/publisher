5cb2507cde2072af1153ef870ed43106ce32a781
isabelizimm <isabel.zimmerman@rstudio.com>
2023-05-03 11:52:59 -0400

    rm rsconnect.js

--- extensions/jupyter-notebook/build/lib/connect_publishing/__init__.py
+++ extensions/jupyter-notebook/build/lib/connect_publishing/__init__.py
@@ -30,7 +30,7 @@ from rsconnect.http_support import CookieJar
 
 from ssl import SSLError
 
-from connect_publishing.managers import get_model
+#from connect_publishing.managers import get_model
 
 try:
     from connect_publishing.version import version as __version__  # noqa
@@ -73,96 +73,96 @@ def md5(s):
     return h.hexdigest()
 
 
-# https://github.com/jupyter/notebook/blob/master/notebook/base/handlers.py
-class EndpointHandler(APIHandler):
-    @web.authenticated
-    async def post(self, action):
-        data = self.get_json_body()
+# # https://github.com/jupyter/notebook/blob/master/notebook/base/handlers.py
+# class EndpointHandler(APIHandler):
+#     @web.authenticated
+#     async def post(self, action):
+#         data = self.get_json_body()
 
-        if action == "verify_server":
-            server_address = data["server_address"]
-            api_key = data["api_key"]
-            disable_tls_check = data["disable_tls_check"]
-            cadata = data.get("cadata", None)
+#         if action == "verify_server":
+#             server_address = data["server_address"]
+#             api_key = data["api_key"]
+#             disable_tls_check = data["disable_tls_check"]
+#             cadata = data.get("cadata", None)
 
-            canonical_address = None
-            result = None
-            try:
-                canonical_address, result = test_server(
-                    RSConnectServer(server_address, api_key, disable_tls_check, cadata)
-                )
-            except SSLError as exc:
-                if exc.reason == "UNKNOWN_PROTOCOL":
-                    raise web.HTTPError(
-                        400,
-                        'Received an "SSL:UNKNOWN_PROTOCOL" error when trying to connect securely '
-                        + "to the Posit Connect server.\n"
-                        + '* Try changing "https://" in the "Server Address" field to "http://".\n'
-                        + "* If the condition persists, contact your Posit Connect server "
-                        + "administrator.",
-                    )
-                raise web.HTTPError(
-                    400,
-                    "A TLS error occurred when trying to reach the Posit Connect server.\n"
-                    + "* Ensure that the server address you entered is correct.\n"
-                    + "* Ask your Posit Connect administrator if you need a certificate bundle and\n"
-                    + '  upload it using "Upload TLS Certificate Bundle" below.',
-                )
-            except Exception as err:
-                self.log.exception("Unable to verify that the provided server is running Posit Connect")
-                raise web.HTTPError(
-                    400,
-                    "Unable to verify that the provided server is running Posit Connect: %s" % err,
-                )
-            if canonical_address is not None:
-                uri = canonical_address.url
-                try:
-                    verify_api_key(RSConnectServer(uri, api_key, disable_tls_check, cadata))
-                    address_hash = md5(server_address)
-                    self.finish(
-                        json.dumps(
-                            {
-                                "status": "Provided server is running Posit Connect",
-                                "address_hash": address_hash,
-                                "server_address": canonical_address.url,
-                            }
-                        )
-                    )
-                except RSConnectException:
-                    raise web.HTTPError(401, "Unable to verify the provided API key")
-            return
+#             canonical_address = None
+#             result = None
+#             try:
+#                 canonical_address, result = test_server(
+#                     RSConnectServer(server_address, api_key, disable_tls_check, cadata)
+#                 )
+#             except SSLError as exc:
+#                 if exc.reason == "UNKNOWN_PROTOCOL":
+#                     raise web.HTTPError(
+#                         400,
+#                         'Received an "SSL:UNKNOWN_PROTOCOL" error when trying to connect securely '
+#                         + "to the Posit Connect server.\n"
+#                         + '* Try changing "https://" in the "Server Address" field to "http://".\n'
+#                         + "* If the condition persists, contact your Posit Connect server "
+#                         + "administrator.",
+#                     )
+#                 raise web.HTTPError(
+#                     400,
+#                     "A TLS error occurred when trying to reach the Posit Connect server.\n"
+#                     + "* Ensure that the server address you entered is correct.\n"
+#                     + "* Ask your Posit Connect administrator if you need a certificate bundle and\n"
+#                     + '  upload it using "Upload TLS Certificate Bundle" below.',
+#                 )
+#             except Exception as err:
+#                 self.log.exception("Unable to verify that the provided server is running Posit Connect")
+#                 raise web.HTTPError(
+#                     400,
+#                     "Unable to verify that the provided server is running Posit Connect: %s" % err,
+#                 )
+#             if canonical_address is not None:
+#                 uri = canonical_address.url
+#                 try:
+#                     verify_api_key(RSConnectServer(uri, api_key, disable_tls_check, cadata))
+#                     address_hash = md5(server_address)
+#                     self.finish(
+#                         json.dumps(
+#                             {
+#                                 "status": "Provided server is running Posit Connect",
+#                                 "address_hash": address_hash,
+#                                 "server_address": canonical_address.url,
+#                             }
+#                         )
+#                     )
+#                 except RSConnectException:
+#                     raise web.HTTPError(401, "Unable to verify the provided API key")
+#             return
 
-        if action == "app_search":
-            uri = data["server_address"]
-            api_key = data["api_key"]
-            title = data["notebook_title"]
-            app_id = data.get("app_id")
-            disable_tls_check = data["disable_tls_check"]
-            cadata = data.get("cadata", None)
+#         if action == "app_search":
+#             uri = data["server_address"]
+#             api_key = data["api_key"]
+#             title = data["notebook_title"]
+#             app_id = data.get("app_id")
+#             disable_tls_check = data["disable_tls_check"]
+#             cadata = data.get("cadata", None)
 
-            try:
-                server = RSConnectServer(uri, api_key, disable_tls_check, cadata)
-                retval = override_title_search(server, app_id, title)
-            except RSConnectException as exc:
-                raise web.HTTPError(400, exc.message)
-            self.finish(json.dumps(retval))
-            return
+#             try:
+#                 server = RSConnectServer(uri, api_key, disable_tls_check, cadata)
+#                 retval = override_title_search(server, app_id, title)
+#             except RSConnectException as exc:
+#                 raise web.HTTPError(400, exc.message)
+#             self.finish(json.dumps(retval))
+#             return
 
-        if action == "get_python_settings":
-            uri = data["server_address"]
-            api_key = data["api_key"]
-            disable_tls_check = data["disable_tls_check"]
-            cadata = data.get("cadata", None)
+#         if action == "get_python_settings":
+#             uri = data["server_address"]
+#             api_key = data["api_key"]
+#             disable_tls_check = data["disable_tls_check"]
+#             cadata = data.get("cadata", None)
 
-            try:
-                server = RSConnectServer(uri, api_key, disable_tls_check, cadata)
-                with RSConnect(server) as api_client:
-                    retval = api_client.python_settings()
-                server.handle_bad_response(retval)
-            except RSConnectException as exc:
-                raise web.HTTPError(400, exc.message)
-            self.finish(json.dumps(retval))
-            return
+#             try:
+#                 server = RSConnectServer(uri, api_key, disable_tls_check, cadata)
+#                 with RSConnect(server) as api_client:
+#                     retval = api_client.python_settings()
+#                 server.handle_bad_response(retval)
+#             except RSConnectException as exc:
+#                 raise web.HTTPError(400, exc.message)
+#             self.finish(json.dumps(retval))
+#             return
 
     @web.authenticated
     def get(self, action):
