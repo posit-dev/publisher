d85df9c0ec25709858248813d6b77bca02bb4a6b
isabelizimm <isabel.zimmerman@rstudio.com>
2023-05-04 10:47:42 -0400

    rm md5

--- extensions/jupyter-notebook/connect_publishing/__init__.py
+++ extensions/jupyter-notebook/connect_publishing/__init__.py
@@ -55,57 +55,57 @@ def _jupyter_nbextension_paths():
     ]
 
 
-def md5(s):
-    if hasattr(s, "encode"):
-        s = s.encode("utf-8")
+# def md5(s):
+#     if hasattr(s, "encode"):
+#         s = s.encode("utf-8")
 
-    try:
-        h = hashlib.md5()
-    except Exception:
-        # md5 is not available in FIPS mode, see if the usedforsecurity option is available
-        # (it was added in python 3.9). We set usedforsecurity=False since we are only
-        # using this for a file upload integrity check.
-        h = hashlib.md5(usedforsecurity=False)
+#     try:
+#         h = hashlib.md5()
+#     except Exception:
+#         # md5 is not available in FIPS mode, see if the usedforsecurity option is available
+#         # (it was added in python 3.9). We set usedforsecurity=False since we are only
+#         # using this for a file upload integrity check.
+#         h = hashlib.md5(usedforsecurity=False)
 
-    h.update(s)
-    return h.hexdigest()
+#     h.update(s)
+#     return h.hexdigest()
 
 
-# https://github.com/jupyter/notebook/blob/master/notebook/base/handlers.py
-class EndpointHandler(APIHandler):
-    @web.authenticated
-    async def post(self, action):
-        data = self.get_json_body()
+# # https://github.com/jupyter/notebook/blob/master/notebook/base/handlers.py
+# class EndpointHandler(APIHandler):
+#     @web.authenticated
+#     async def post(self, action):
+#         data = self.get_json_body()
 
-        if action == "get_python_settings":
-            uri = data["server_address"]
-            api_key = data["api_key"]
-            disable_tls_check = data["disable_tls_check"]
-            cadata = data.get("cadata", None)
+#         if action == "get_python_settings":
+#             uri = data["server_address"]
+#             api_key = data["api_key"]
+#             disable_tls_check = data["disable_tls_check"]
+#             cadata = data.get("cadata", None)
 
-            try:
-                server = RSConnectServer(uri, api_key, disable_tls_check, cadata)
-                with RSConnect(server) as api_client:
-                    retval = api_client.python_settings()
-                server.handle_bad_response(retval)
-            except RSConnectException as exc:
-                raise web.HTTPError(400, exc.message)
-            self.finish(json.dumps(retval))
-            return
+#             try:
+#                 server = RSConnectServer(uri, api_key, disable_tls_check, cadata)
+#                 with RSConnect(server) as api_client:
+#                     retval = api_client.python_settings()
+#                 server.handle_bad_response(retval)
+#             except RSConnectException as exc:
+#                 raise web.HTTPError(400, exc.message)
+#             self.finish(json.dumps(retval))
+#             return
 
-    @web.authenticated
-    def get(self, action):
-        if action == "plugin_version":
-            connect_publishing_server_extension = __version__
-            rsconnect_python_version = VERSION
-            self.finish(
-                json.dumps(
-                    {
-                        "connect_publishing server extension version": connect_publishing_server_extension,
-                        "rsconnect_python version": rsconnect_python_version,
-                    }
-                )
-            )
+#     @web.authenticated
+#     def get(self, action):
+#         if action == "plugin_version":
+#             connect_publishing_server_extension = __version__
+#             rsconnect_python_version = VERSION
+#             self.finish(
+#                 json.dumps(
+#                     {
+#                         "connect_publishing server extension version": connect_publishing_server_extension,
+#                         "rsconnect_python version": rsconnect_python_version,
+#                     }
+#                 )
+#             )
 
 
 def load_jupyter_server_extension(nb_app):
@@ -114,4 +114,4 @@ def load_jupyter_server_extension(nb_app):
     host_pattern = ".*$"
     action_pattern = r"(?P<action>\w+)"
     route_pattern = url_path_join(web_app.settings["base_url"], r"/connect_publishing/%s" % action_pattern)
-    web_app.add_handlers(host_pattern, [(route_pattern, EndpointHandler)])
+    #web_app.add_handlers(host_pattern, [(route_pattern, EndpointHandler)])
