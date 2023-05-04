79a3881752c65defad8225b418d0c7f6b1ef5cec
Michael Marchetti <mike@rstudio.com>
2023-05-04 15:58:56 -0400

    remove util.Size

--- internal/util/files.go
+++ internal/util/files.go
@@ -3,32 +3,11 @@ package util
 // Copyright (C) 2023 by Posit Software, PBC.
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/spf13/afero"
 )
-
-type Size int64
-
-func (n Size) String() string {
-	if n < 1e3 {
-		return fmt.Sprintf("%d", n)
-	} else if n < 1e6 {
-		return fmt.Sprintf("%.1f KB", float64(n)/1e3)
-	} else if n < 1e9 {
-		return fmt.Sprintf("%.1f MB", float64(n)/1e6)
-	} else if n < 1e12 {
-		return fmt.Sprintf("%.1f GB", float64(n)/1e9)
-	} else {
-		return fmt.Sprintf("%.1f TB", float64(n)/1e12)
-	}
-}
-
-func (n Size) ToInt64() int64 {
-	return int64(n)
-}
 
 func Chdir(dir string) (string, error) {
 	oldWd, err := os.Getwd()
