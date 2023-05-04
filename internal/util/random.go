6688fe9c3a86ebe984811ba860081c1e324770bd
Michael Marchetti <mike@rstudio.com>
2023-05-04 16:03:16 -0400

    test for util/random.go

--- internal/util/random.go
+++ internal/util/random.go
@@ -2,10 +2,12 @@ package util
 
 // Copyright (C) 2023 by Posit Software, PBC.
 
-import "crypto/rand"
+import (
+	"crypto/rand"
+)
 
 func RandomBytes(n int) ([]byte, error) {
-	buf := make([]byte, 32)
+	buf := make([]byte, n)
 	_, err := rand.Read(buf)
 	return buf, err
 }
---
+++ internal/util/random_test.go
@@ -0,0 +1,24 @@
+package util
+
+// Copyright (C) 2023 by Posit Software, PBC.
+
+import (
+	"testing"
+
+	"github.com/rstudio/connect-client/internal/util/utiltest"
+	"github.com/stretchr/testify/suite"
+)
+
+type RandomSuite struct {
+	utiltest.Suite
+}
+
+func TestRandomSuite(t *testing.T) {
+	suite.Run(t, new(RandomSuite))
+}
+
+func (s *RandomSuite) TestRandomBytes() {
+	r, err := RandomBytes(32)
+	s.Nil(err)
+	s.Len(r, 32)
+}
