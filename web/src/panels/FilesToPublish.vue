57f8dff89b66c494649e14d622e0c710d45a11bd
Jordan Jensen <jordan.jensen@rstudio.com>
2023-08-07 15:24:00 -0700

    Simplest files tree using files API

--- web/src/panels/FilesToPublish.vue
+++ web/src/panels/FilesToPublish.vue
@@ -17,8 +17,42 @@
 
     <q-card class="bg-grey-9">
       <q-card-section>
-        TODO: file selection for deployment
+        <q-tree
+          :nodes="files"
+          node-key="key"
+          tick-strategy="leaf"
+          dark
+          dense
+        />
       </q-card-section>
     </q-card>
   </q-expansion-item>
 </template>
+
+<script setup lang="ts">
+import type { QTree, QTreeNode } from 'quasar';
+import { ref } from 'vue';
+
+import { useApi, DeploymentFile } from 'src/api';
+
+const api = useApi();
+
+const files = ref<QTreeNode[]>([]);
+
+function fileToTreeNode(file: DeploymentFile): QTreeNode {
+  const node: QTreeNode = {
+    key: file.pathname,
+    label: file.pathname,
+    children: file.files.map(fileToTreeNode),
+  };
+
+  return node;
+}
+
+async function getFiles() {
+  const response = await api.files.get({ pathname: 'web/src/api' });
+  files.value = [fileToTreeNode(response.data)];
+}
+
+getFiles();
+</script>
