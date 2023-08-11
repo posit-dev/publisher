35557d91ee906be7d0842d7834a4397cc0ab297a
Jordan Jensen <jordan.jensen@rstudio.com>
2023-08-11 16:28:14 -0700

    Add debounced function in store for PUT files api

--- web/src/stores/deployment.ts
+++ web/src/stores/deployment.ts
@@ -2,13 +2,36 @@
 
 import { CanceledError } from 'axios';
 import { defineStore } from 'pinia';
-import { computed, ref } from 'vue';
+import { Ref, computed, ref } from 'vue';
 
 import api, { Deployment } from 'src/api';
 import { CancelController } from 'src/api/utils/CancelController';
 import { deploymentToPathnames, pathnamesToManifestFiles } from 'src/api/utils';
+import { debounce } from 'quasar';
 
 const fileSyncCancelController = new CancelController();
+
+/**
+ * Set the files on the server side.
+ * Debounced to avoid API calls on every state change.
+ * In progress API calls are cancelled to avoid de-syncing the deployment state.
+ */
+const setFilesOnServer = debounce((
+  files: string[],
+  deploymentState: Ref<Deployment | undefined>
+) => fileSyncCancelController.cancelPrevious(async() => {
+  try {
+    const { data } = await api.deployment.setFiles(
+      files,
+      { signal: fileSyncCancelController.signal }
+    );
+    deploymentState.value = data;
+  } catch (err) {
+    if (err instanceof CanceledError) {
+      // ignore
+    }
+  }
+}), 0);
 
 export const useDeploymentStore = defineStore('deployment', () => {
   const deployment = ref<Deployment>();
@@ -16,23 +39,12 @@ export const useDeploymentStore = defineStore('deployment', () => {
   const files = computed<string[]>({
     get: () => deploymentToPathnames(deployment.value),
     set: async(selectedFiles) => {
+      // Update the deployment state immediately before sending to server
       if (deployment.value) {
         deployment.value.manifest.files = pathnamesToManifestFiles(selectedFiles);
       }
 
-      fileSyncCancelController.cancelPrevious(async() => {
-        try {
-          const { data } = await api.deployment.setFiles(
-            selectedFiles,
-            { signal: fileSyncCancelController.signal }
-          );
-          deployment.value = data;
-        } catch (err) {
-          if (err instanceof CanceledError) {
-            // ignore
-          }
-        }
-      });
+      setFilesOnServer(selectedFiles, deployment);
     }
   });
 
