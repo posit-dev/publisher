c6f106833f8dc2fe8a0f767bfb3aa883cf6c3145
Jordan Jensen <jordan.jensen@rstudio.com>
2023-08-10 17:56:19 -0700

    Create deployment state files computed get/set

--- web/src/stores/deployment.ts
+++ web/src/stores/deployment.ts
@@ -1,14 +1,23 @@
 // Copyright (C) 2023 by Posit Software, PBC.
 
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
-import { Deployment } from 'src/api';
+import api, { Deployment } from 'src/api';
 
 export const useDeploymentStore = defineStore('deployment', () => {
   const deployment = ref<Deployment>();
 
+  const files = computed<string[]>({
+    get: () => Object.keys(deployment.value?.manifest.files ?? {}),
+    set: async(selectedFiles) => {
+      const { data } = await api.deployment.setFiles(selectedFiles);
+      deployment.value = data;
+    }
+  });
+
   return {
-    deployment
+    deployment,
+    files
   };
 });
