6a567133fd882cfa71ddb78bc7aaa4607782a08d
Jordan Jensen <jordan.jensen@rstudio.com>
2023-08-14 12:38:49 -0700

    Generically type requestOnce function

--- web/src/utils/requestOnce.ts
+++ web/src/utils/requestOnce.ts
@@ -1,6 +1,12 @@
 import { debounce } from 'quasar';
 
-export function requestOnce(fn, wait: number) {
+type SignalFn = (signal: AbortSignal, ...args: unknown[]) => unknown;
+type ExcludeSignal<T extends unknown[]> = T extends [AbortSignal, ...infer P] ? P : never;
+
+export function requestOnce<F extends SignalFn>(
+  fn: F,
+  wait: number
+): (...args: ExcludeSignal<Parameters<F>>) => void {
   let controller: AbortController | undefined;
 
   return debounce((...args) => {
