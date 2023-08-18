0a2cbf1560ff6462fd586b6d86fb910c0af9ab0a
Jordan Jensen <jordan.jensen@rstudio.com>
2023-08-18 12:28:02 -0700

    Create casing interceptors for api module

--- web/src/api/client.ts
+++ web/src/api/client.ts
@@ -1,11 +1,27 @@
 // Copyright (C) 2023 by Posit Software, PBC.
 
-import axios from 'axios';
+import axios, { InternalAxiosRequestConfig, AxiosResponse } from 'axios';
+import camelcaseKeys from 'camelcase-keys';
+import decamelizeKeys from 'decamelize-keys';
 
 import { Accounts } from 'src/api/resources/Accounts';
 import { Deployment } from 'src/api/resources/Deployment';
 import { Files } from 'src/api/resources/Files';
 import { Publish } from 'src/api/resources/Publish';
+
+const camelCaseInterceptor = (response: AxiosResponse): AxiosResponse => {
+  if (response.data && response.headers['content-type'] === 'application/json') {
+    response.data = camelcaseKeys(response.data, { deep: true });
+  }
+  return response;
+};
+
+const snakeCaseInterceptor = (config: InternalAxiosRequestConfig) => {
+  if (config.data) {
+    config.data = decamelizeKeys(config.data, { deep: true });
+  }
+  return config;
+};
 
 class PublishingClientApi {
   accounts: Accounts;
@@ -18,6 +34,9 @@ class PublishingClientApi {
       baseURL: '/api',
       withCredentials: true,
     });
+
+    client.interceptors.request.use(snakeCaseInterceptor);
+    client.interceptors.response.use(camelCaseInterceptor);
 
     this.accounts = new Accounts(client);
     this.deployment = new Deployment(client);
