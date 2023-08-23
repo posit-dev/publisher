d07628c542f5c9691eae34e759d6a06381cb9f00
Jordan Jensen <jordan.jensen@rstudio.com>
2023-08-23 14:09:31 -0700

    Use bare keys and add comments to eslintrc

--- web2/.eslintrc.cjs
+++ web2/.eslintrc.cjs
@@ -1,51 +1,62 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:vue/vue3-essential'
-    ],
-    'overrides': [
-        {
-            'env': {
-                'node': true
-            },
-            'files': [
-                '.eslintrc.{js,cjs}'
-            ],
-            'parserOptions': {
-                'sourceType': 'script'
-            }
-        }
-    ],
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'parser': '@typescript-eslint/parser',
-        'sourceType': 'module'
-    },
-    'plugins': [
-        '@typescript-eslint',
-        'vue'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
+  env: {
+    browser: true,
+    es2021: true
+  },
+
+  extends: [
+    // Core rules for common problems
+    // https://eslint.org/docs/latest/use/configure/configuration-files#using-eslintrecommended
+    'eslint:recommended',
+
+    // Typescript recommended rules
+    // https://typescript-eslint.io/linting/configs#recommended-configurations
+    'plugin:@typescript-eslint/recommended',
+
+    // Includes vue3-essential and vue3-strongly-recommended
+    // https://eslint.vuejs.org/user-guide/#bundle-configurations
+    'plugin:vue/vue3-recommended'
+  ],
+
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
     }
+  ],
+
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+
+  plugins: [
+    '@typescript-eslint',
+    'vue'
+  ],
+
+  rules: {
+    indent: [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'always'
+    ]
+  }
 };
