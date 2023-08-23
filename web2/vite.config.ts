0e999c6584becea912cc6c57445c4d354e736818
Jordan Jensen <jordan.jensen@rstudio.com>
2023-08-23 16:06:34 -0700

    Run eslint --fix

--- web2/vite.config.ts
+++ web2/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line no-restricted-syntax
 export default defineConfig({
   plugins: [vue()],
-})
+});
