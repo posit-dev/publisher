1c28dc5791b82103111697c7b3036dbfd6a80086
Jordan Jensen <jordan.jensen@rstudio.com>
2023-08-23 16:53:38 -0700

    Implement debounce without quasar import

---
+++ web2/src/utils/debounce.ts
@@ -0,0 +1,18 @@
+// Copyright (C) 2023 by Posit Software, PBC.
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type DebounceFn = (...args: any[]) => unknown;
+
+export function debounce<F extends DebounceFn>(
+  fn: F,
+  wait?: number
+): ((...args: Parameters<F>) => void) {
+  let timeoutId: number | undefined;
+
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      fn(...args);
+    }, wait);
+  };
+}
