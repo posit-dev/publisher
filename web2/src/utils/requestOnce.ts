4a2f70aa3ad70cd6386ddb8554402e9b2a401400
Jordan Jensen <jordan.jensen@rstudio.com>
2023-08-23 16:53:53 -0700

    Add requestOnce util

---
+++ web2/src/utils/requestOnce.ts
@@ -0,0 +1,22 @@
+// Copyright (C) 2023 by Posit Software, PBC.
+
+import { debounce } from 'src/utils/debounce';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type SignalFn = (signal: AbortSignal, ...args: any[]) => unknown;
+type ExcludeSignal<T extends unknown[]> = T extends [AbortSignal, ...infer P] ? P : never;
+
+export function requestOnce<F extends SignalFn>(
+  fn: F,
+  wait: number
+): (...args: ExcludeSignal<Parameters<F>>) => void {
+  let controller: AbortController | undefined;
+
+  return debounce((...args) => {
+    if (controller) {
+      controller.abort();
+    }
+    controller = new AbortController();
+    fn(controller.signal, ...args);
+  }, wait);
+}
