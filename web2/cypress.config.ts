e435a2d8687594cb866dff917cee19002b4b1031
Jordan Jensen <jordan.jensen@rstudio.com>
2023-08-24 15:13:50 -0700

    Setup Cypress with Cypress launchpad

---
+++ web2/cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress';
+
+// eslint-disable-next-line no-restricted-syntax
+export default defineConfig({
+  e2e: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setupNodeEvents(_on, _config) {
+      // implement node event listeners here
+    },
+  },
+});
