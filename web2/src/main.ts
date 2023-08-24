11adf4f95f1719fbc5b0dcbaa33f459e73ad76cf
Jordan Jensen <jordan.jensen@rstudio.com>
2023-08-24 12:09:27 -0700

    Install @quasar/extras for font and material icons

--- web2/src/main.ts
+++ web2/src/main.ts
@@ -2,6 +2,13 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { Quasar } from 'quasar';
 
+// Import Quasar Roboto Font
+import '@quasar/extras/roboto-font/roboto-font.css';
+
+// Import Material Icons
+import '@quasar/extras/material-icons/material-icons.css';
+
+// Import Quasar CSS
 import 'quasar/dist/quasar.css';
 
 import './style.css';
