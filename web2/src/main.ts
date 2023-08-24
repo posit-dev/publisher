8c547f0bb9dc9aafa2fa895f716abecc732ba5cc
Jordan Jensen <jordan.jensen@rstudio.com>
2023-08-23 17:15:14 -0700

    Install Quasar Vite plugin

--- web2/src/main.ts
+++ web2/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { Quasar } from 'quasar';
+
+import 'quasar/dist/quasar.css';
 
 import './style.css';
 import App from './App.vue';
@@ -8,4 +11,5 @@ const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
+app.use(Quasar, {});
 app.mount('#app');
--- web2/vite.config.ts
+++ web2/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vitejs.dev/config/
 // eslint-disable-next-line no-restricted-syntax
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    quasar()
+  ],
 });
