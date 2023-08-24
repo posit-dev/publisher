f37b8b411e7257e74a4658737fda48ebf024f064
Jordan Jensen <jordan.jensen@rstudio.com>
2023-08-24 11:31:31 -0700

    Add panel components and PublishProcess

---
+++ web2/src/components/panels/AdvancedSettings.vue
@@ -0,0 +1,15 @@
+<!-- Copyright (C) 2023 by Posit Software, PBC. -->
+
+<template>
+  <LayoutPanel
+    title="Advanced Settings"
+    icon="img:assets/images/settings.png"
+  >
+    TODO: Show list of editable settings (and some as read-only)
+    based on capabilities of target server.
+  </LayoutPanel>
+</template>
+
+<script setup lang="ts">
+import LayoutPanel from 'src/components/LayoutPanel.vue';
+</script>
---
+++ web2/src/components/panels/CommonSettings.vue
@@ -0,0 +1,15 @@
+<!-- Copyright (C) 2023 by Posit Software, PBC. -->
+
+<template>
+  <LayoutPanel
+    title="Common Settings"
+    icon="img:assets/images/info.png"
+  >
+    TODO: Common settings, probably off the info panel in dashboard
+  </LayoutPanel>
+</template>
+
+<script setup lang="ts">
+import LayoutPanel from 'src/components/LayoutPanel.vue';
+</script>
+
