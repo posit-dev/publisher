1668add6bd542b15615331bc69ce16d7d5ddc273
Jordan Jensen <jordan.jensen@rstudio.com>
2023-08-24 15:17:02 -0700

    Bring in home.cy.ts Cypress tests

--- web2/cypress/e2e/home.cy.ts
+++ web2/cypress/e2e/home.cy.ts
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
+describe('Landing', () => {
+  beforeEach(() => {
+    cy.visit('');
+  });
+  it('.should() - assert that <title> is correct', () => {
+    cy.title().should('include', 'Posit Publishing Web UI');
+  });
+});
+
+describe('Check Files', () => {
+  beforeEach(() => {
+    cy.visit('');
+  });
+  it('files should be listed', () => {
+    cy.contains('.q-item__label', 'Files').should('be.visible')
+      .click();
+    cy.get('.q-tree__node-header-content').contains('fastapi-simple');
   });
 });
