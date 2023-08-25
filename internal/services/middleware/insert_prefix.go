22187571ccb2a05f78b4169daade9ebd17426250
tdstein <taylor.steinberg@posit.co>
2023-08-25 16:10:15 -0400

    Removes unnecessary branching logic.

--- internal/services/middleware/insert_prefix.go
+++ internal/services/middleware/insert_prefix.go
@@ -13,18 +13,12 @@ func InsertPrefix(h http.Handler, prefix string) http.Handler {
 		return h
 	}
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		p := prefix + r.URL.Path
-		rp := prefix + r.URL.RawPath
-		if len(p) > len(r.URL.Path) && (r.URL.RawPath == "" || len(rp) > len(r.URL.RawPath)) {
-			r2 := new(http.Request)
-			*r2 = *r
-			r2.URL = new(url.URL)
-			*r2.URL = *r.URL
-			r2.URL.Path = p
-			r2.URL.RawPath = rp
-			h.ServeHTTP(w, r2)
-		} else {
-			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		}
+		r2 := new(http.Request)
+		*r2 = *r
+		r2.URL = new(url.URL)
+		*r2.URL = *r.URL
+		r2.URL.Path = prefix + r.URL.Path
+		r2.URL.RawPath = prefix + r.URL.RawPath
+		h.ServeHTTP(w, r2)
 	})
 }
