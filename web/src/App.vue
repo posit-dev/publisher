061edc252ae141a02bc4292ac09d397eb8900223
Bill Sager <whs@sager.org>
2023-08-25 11:00:12 -0700

    Use debug.console rather than logging class.

--- web/src/App.vue
+++ web/src/App.vue
@@ -8,10 +8,8 @@
 import { onMounted } from 'vue';
 import { useApi } from 'src/api';
 import { useDeploymentStore } from 'src/stores/deployment';
-import { useLogger } from './utils/logger';
 import { useRouter, useRoute } from 'vue-router';
 
-const logger = useLogger();
 const router = useRouter();
 const route = useRoute();
 
@@ -27,10 +25,18 @@ const handleQueryParams = async() => {
   // router is async so we wait for it to be ready
   await router.isReady();
 
-  // debug
+  // Enable debug logging output only when we've got this query flag
+  // example: http://127.0.0.1:9000/#/?debug
+  // note: will not work if initial URL is http://127.0.0.1:9000/?debug
+  //       as it becomes http://127.0.0.1:9000/?debug=true#/
   if ('debug' in route.query) {
-    logger.enableLogging('DEBUG');
+    console.debug = console.log.bind(window.console);
+  } else {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    console.debug = function(){};
   }
+  // This will only output a debug console message if we received the query flag.
+  console.debug('Debug output messages are active!');
 };
 
 onMounted(async() => {
