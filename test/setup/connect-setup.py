25f47f517f3dc83bad95147b52ac80c8b5148a08
Kevin Gartland <kgartland@rstudio.com>
2023-09-12 16:33:13 -0400

    fix script

--- test/setup/connect-setup.py
+++ test/setup/connect-setup.py
@@ -70,12 +70,11 @@ def connect_ready(box_name, max_attempts, interval):
         attempts += 1
     return None
 
+response = connect_ready(box_name, 20, 5)
 api_key=get_api_key('admin')
 latest_connect=get_latest_connect_version()
 current_connect=get_current_connect_version(get_ip(box_name), api_key)
 install_connect = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu@" + get_ip(box_name) + " " + ssh_options + " sudo -E UNATTENDED=1 bash installer-ci.sh " + latest_connect
-
-response = connect_ready(box_name, 20, 5)
 
 if response:
     print(get_ip(box_name))
