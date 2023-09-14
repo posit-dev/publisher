47bacb68e65a7df85529e0e4f1dac88111646065
Michael Marchetti <mike@rstudio.com>
2023-09-14 07:40:06 -0400

    add to notebook toolbar

--- extensions/jupyterlab/connect_jupyterlab/src/index.ts
+++ extensions/jupyterlab/connect_jupyterlab/src/index.ts
@@ -3,6 +3,10 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import {
+  ICommandPalette,
+} from '@jupyterlab/apputils';
+
 import { requestAPI } from './handler';
 
 /**
@@ -12,18 +16,41 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'connect_jupyterlab:plugin',
   description: 'A JupyterLab extension for publishing to Posit Connect.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette],
+  activate: (
+    app: JupyterFrontEnd,
+    palette: ICommandPalette) => {
     console.log('JupyterLab extension connect_jupyterlab is activated!');
+    const { commands } = app;
 
-    requestAPI<any>('get-example')
-      .then(data => {
-        console.log(data);
-      })
-      .catch(reason => {
-        console.error(
-          `The connect_jupyterlab server extension appears to be missing.\n${reason}`
+    const command = 'posit:publish';
+
+    // Add a command
+    commands.addCommand(command, {
+      label: 'Publish',
+      caption: 'Publish to Posit Connect',
+      iconClass: 'rsc-icon',
+      execute: (args: any) => {
+        console.log(
+          `${command} command has been called from ${args['origin']}.`
         );
-      });
+      }
+    });
+
+    // Add the command to the command palette
+    const category = 'notebook';
+    palette.addItem({ command, category, args: { origin: 'palette' } });
+
+    // Example API request, doesn't belong here.
+    requestAPI<any>('get-example')
+    .then(data => {
+      console.log(data);
+    })
+    .catch(reason => {
+      console.error(
+        `The connect_jupyterlab server extension appears to be missing.\n${reason}`
+      );
+    });
   }
 };
 
