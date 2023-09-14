7100d21bb79e78f97c6d42ddcbc4d475733826bd
Michael Marchetti <mike@rstudio.com>
2023-09-14 12:37:11 -0400

    handler cleanup

--- extensions/jupyterlab/connect_jupyterlab/connect_jupyterlab/handlers.py
+++ extensions/jupyterlab/connect_jupyterlab/connect_jupyterlab/handlers.py
@@ -4,21 +4,20 @@ from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
 
+
 class RouteHandler(APIHandler):
-    # The following decorator should be present on all verb methods (head, get, post,
-    # patch, put, delete, options) to ensure only authorized user can request the
-    # Jupyter server
+    # The following decorator should be present on all verb methods
+    # (head, get, post, patch, put, delete, options) to ensure
+    # only authorized user can request the Jupyter server
     @tornado.web.authenticated
-    def get(self):
-        self.finish(json.dumps({
-            "data": "This is /connect-jupyterlab/get-example endpoint!"
-        }))
+    def post(self):
+        self.finish(json.dumps({"data": "POST /connect-jupyterlab/publish endpoint!"}))
 
 
 def setup_handlers(web_app):
     host_pattern = ".*$"
 
     base_url = web_app.settings["base_url"]
-    route_pattern = url_path_join(base_url, "connect-jupyterlab", "get-example")
+    route_pattern = url_path_join(base_url, "connect-jupyterlab", "publish")
     handlers = [(route_pattern, RouteHandler)]
     web_app.add_handlers(host_pattern, handlers)
