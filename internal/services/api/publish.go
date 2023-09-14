5dcfb4fe08f90df15f0f7bba7c2df79a99cdae8d
Michael Marchetti <mike@rstudio.com>
2023-09-14 08:20:20 -0400

    make publish API async

--- internal/services/api/publish.go
+++ internal/services/api/publish.go
@@ -3,24 +3,41 @@ package api
 // Copyright (C) 2023 by Posit Software, PBC.
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/rstudio/connect-client/internal/accounts"
 	"github.com/rstudio/connect-client/internal/cli_types"
 	"github.com/rstudio/connect-client/internal/logging"
 	"github.com/rstudio/connect-client/internal/publish"
+	"github.com/rstudio/connect-client/internal/state"
 )
+
+type PublishReponse struct {
+	LocalID state.LocalDeploymentID `json:"local_id"` // Unique ID of this publishing operation. Only valid for this run of the agent.
+}
 
 func PostPublishHandlerFunc(publishArgs *cli_types.PublishArgs, lister accounts.AccountList, log logging.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		switch req.Method {
-		case http.MethodPost:
-			err := publish.PublishManifestFiles(publishArgs, lister, log)
-			if err != nil {
-				InternalError(w, req, log, err)
-			}
-		default:
+		localID, err := state.NewLocalID()
+		if err != nil {
+			InternalError(w, req, log, err)
 			return
 		}
+		publishArgs.State.LocalID = localID
+		response := PublishReponse{
+			LocalID: localID,
+		}
+		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(http.StatusAccepted)
+		json.NewEncoder(w).Encode(response)
+
+		go func() {
+			log = log.WithArgs("local_id", localID)
+			err := publish.PublishManifestFiles(publishArgs, lister, log)
+			if err != nil {
+				log.Error("Deployment failed", "error", err.Error())
+			}
+		}()
 	}
 }
--- internal/util/random.go
+++ internal/util/random.go
@@ -4,10 +4,35 @@ package util
 
 import (
 	"crypto/rand"
+	"encoding/base64"
+	"strings"
 )
 
 func RandomBytes(n int) ([]byte, error) {
 	buf := make([]byte, n)
 	_, err := rand.Read(buf)
 	return buf, err
+}
+
+func RandomString(n int) (string, error) {
+	// Base64 encoding of bytes->string expands length by 1/3
+	key, err := RandomBytes((n * 3) / 4)
+	if err != nil {
+		return "", err
+	}
+	tokenString, err := toBase64(key)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func toBase64(data []byte) (string, error) {
+	var writer strings.Builder
+	encoder := base64.NewEncoder(base64.RawURLEncoding, &writer)
+	_, err := encoder.Write(data)
+	if err != nil {
+		return "", err
+	}
+	return writer.String(), nil
 }
