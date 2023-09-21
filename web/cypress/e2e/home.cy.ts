794d34cddc31f61d89cc4194e3c90fc6fa016959
Kevin Gartland <kgartland@rstudio.com>
2023-09-21 14:14:39 -0400

    separate run for ui tests

--- web/cypress/e2e/home.cy.ts
+++ web/cypress/e2e/home.cy.ts
@@ -23,28 +23,3 @@ describe('Check Files', () => {
     cy.get('.q-tree__node-header-content').should('exist');
   });
 });
-
-describe('Publish', () => {
-  it('hit the publish button', () => {
-    cy.visit('http://127.0.0.1:9000/?token=abc123');
-    cy.get('.block').contains('Publish')
-      .click();
-  });
-});
-
-describe('Check Connect Deployment', () => {
-  it('check deployment', { baseUrl: null }, () => {
-    cy.visit(Cypress.env('CYPRESS_CONNECT_ADDRESS'));
-    cy.get('#username').type('admin');
-    cy.get('#password').type('password');
-    cy.get('button[data-automation="login-panel-submit"]')
-      .click();
-    cy.get('#rs_radio_cop-visibility_editor')
-      .click();
-    cy.get('h1[data-automation="content-list-title"]')
-      .contains('Your Content');
-    cy.get('td[data-automation="content-row-icon-title-cell"]')
-      .contains('Untitled')
-      .click();
-  });
-});
