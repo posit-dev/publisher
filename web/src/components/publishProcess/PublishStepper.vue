04feb36cfc4d30b4221f6e496816ab23fbcde817
Jordan Jensen <jordan.jensen@rstudio.com>
2023-10-05 15:44:07 -0700

    Use step components in stepper

--- web/src/components/publishProcess/PublishStepper.vue
+++ web/src/components/publishProcess/PublishStepper.vue
@@ -2,40 +2,35 @@
 
 <template>
   <q-stepper
-    v-model="activeStep"
+    v-model="openStep"
     vertical
     animated
     flat
     :header-nav="true"
   >
-    <PublishStep
-      :name="1"
-      icon="create_new_folder"
-      summary="Registering the deployment object with the Posit Connect Server."
-      :logs="[
-        'Create Deployment',
-        'Loading rsconnect accounts from /Users/billsager/Library/Preferences/org.R-project.R/R/rsconnect',
-        'Loading rsconnect-python accounts from /Users/billsager/Library/Application Support/rsconnect-python/servers.json'
-      ]"
-    />
-    <PublishStep
-      :name="2"
-      icon="create_new_folder"
-      summary="Registering the deployment object with the Posit Connect Server."
-      :logs="[
-        'Create Deployment',
-        'Loading rsconnect accounts from /Users/billsager/Library/Preferences/org.R-project.R/R/rsconnect',
-        'Loading rsconnect-python accounts from /Users/billsager/Library/Application Support/rsconnect-python/servers.json'
-      ]"
-    />
+    <CreateDeployment :name="1" />
+    <CreateBundle :name="2" />
+    <UploadBundle :name="3" />
+    <DeployBundle :name="4" />
+    <RestorePythonEnvironment :name="5" />
+    <RunContent :name="6" />
+    <WrappingUpDeployment :name="7" />
   </q-stepper>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 
-import PublishStep from 'src/components/publishProcess/PublishStep.vue';
+import {
+  CreateDeployment,
+  CreateBundle,
+  UploadBundle,
+  DeployBundle,
+  RestorePythonEnvironment,
+  RunContent,
+  WrappingUpDeployment
+} from 'src/components/publishProcess/steps';
 
-const activeStep = ref(2);
+const openStep = ref<number | undefined>(undefined);
 </script>
 
