ede03e19c6bebbada418c307b45211f07048ae64
Jordan Jensen <jordan.jensen@rstudio.com>
2023-10-10 23:52:03 -0700

    Use publish/success in WrappingUpDeployment step

--- web/src/components/publishProcess/steps/WrappingUpDeployment.vue
+++ web/src/components/publishProcess/steps/WrappingUpDeployment.vue
@@ -5,20 +5,48 @@
     :name="name"
     title="Wrapping Up Deployment"
     icon="checklist"
-    summary="Your project has been successfully deployed to the server and is available at https://rsc.radixu.com/connect/my-project"
+    :summary="summary"
     :done="done"
   />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onBeforeUnmount, ref } from 'vue';
 
 import PublishStep from 'src/components/publishProcess/PublishStep.vue';
+import { useEventStream } from 'src/plugins/eventStream';
+import { computed } from 'vue';
+import { useDeploymentStore } from 'src/stores/deployment';
+
+const deploymentStore = useDeploymentStore();
+const $eventStream = useEventStream();
 
 defineProps({
   name: { type: [String, Number], required: true },
 });
+const emit = defineEmits(['done']);
 
+const dashboardUrl = ref<string | undefined>(undefined);
+const directUrl = ref<string | undefined>(undefined);
 const done = ref(false);
+
+const summary = computed(() => {
+  const destination = deploymentStore.deployment?.target.accountName;
+  if (done.value) {
+    return `Your project has been successfully published to ${destination} and is available at ${dashboardUrl.value}.`;
+  }
+  return `Your project is still being published...`;
+});
+
+const successCb = $eventStream.addEventMonitorCallback('publish/success', (msg) => {
+  dashboardUrl.value = msg.data.dashboardUrl;
+  directUrl.value = msg.data.directUrl;
+  done.value = true;
+  emit('done');
+});
+
+onBeforeUnmount(() => {
+  $eventStream.delEventFilterCallback(successCb);
+});
 </script>
 
