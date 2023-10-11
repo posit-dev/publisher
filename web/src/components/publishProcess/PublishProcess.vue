52eeec2a1164c52f5456a69a3d816ebb3178cefd
Jordan Jensen <jordan.jensen@rstudio.com>
2023-10-10 23:27:56 -0700

    Remove all events from PublishProcess

--- web/src/components/publishProcess/PublishProcess.vue
+++ web/src/components/publishProcess/PublishProcess.vue
@@ -21,41 +21,21 @@
       </h6>
       <PublishSummary />
       <PublishStepper />
-      <div
-        v-for="(eventItem, index) in events"
-        :key="index"
-      >
-        {{ JSON.stringify(eventItem) }}
-      </div>
-      <p ref="agentLogEnd">
-        &nbsp;
-      </p>
     </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
 
-import { EventStreamMessage, PublishSuccess } from 'src/api/types/events';
-import { computed, PropType, onBeforeUnmount, ref, watch } from 'vue';
-import { scroll as qScroll } from 'quasar';
+import { PublishSuccess } from 'src/api/types/events';
+import { computed, onBeforeUnmount, ref } from 'vue';
 import { useEventStream } from 'src/plugins/eventStream';
 import PublishStepper from 'src/components/publishProcess/PublishStepper.vue';
 import PublishSummary from 'src/components/publishProcess/PublishSummary.vue';
 import { useDeploymentStore } from 'src/stores/deployment';
 
-const { getScrollTarget, setVerticalScrollPosition } = qScroll;
-
-const props = defineProps({
-  events: {
-    type: Array as PropType<EventStreamMessage[]>,
-    required: true,
-  }
-});
-
 const $eventStream = useEventStream();
 const deploymentStore = useDeploymentStore();
-const agentLogEnd = ref<HTMLDivElement | null>(null);
 
 const emit = defineEmits(['back']);
 
@@ -79,16 +59,6 @@ const publishingComplete = (msg: PublishSuccess) => {
   console.log(`msg: ${JSON.stringify(msg)}`);
 };
 $eventStream.addEventMonitorCallback('publish/success', publishingComplete);
-
-watch(props.events, () => {
-  if (agentLogEnd.value) {
-    const el = agentLogEnd.value as HTMLDivElement;
-    const target = getScrollTarget(el);
-    const offset = el.offsetTop;
-    const duration = 1000;
-    setVerticalScrollPosition(target, offset, duration);
-  }
-});
 
 onBeforeUnmount(() => {
   $eventStream.delEventFilterCallback(publishingComplete);
