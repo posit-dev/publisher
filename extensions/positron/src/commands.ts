4a9263c4a83000988c06a012c637e51ecf1a980d
tdstein <taylor.steinberg@posit.co>
2023-11-09 11:07:12 -0500

    Removes --skip-browser-session-auth from command.

--- extensions/positron/src/commands.ts
+++ extensions/positron/src/commands.ts
@@ -3,5 +3,5 @@ const executable: string = "connect-client";
 export type Command = string;
 
 export const create = (port: number, subcommand: string = "publish-ui"): Command => {
-    return `${executable} ${subcommand} --listen=127.0.0.1:${port} test/sample-content/fastapi-simple --skip-browser-session-auth`;
+    return `${executable} ${subcommand} --listen=127.0.0.1:${port} test/sample-content/fastapi-simple`;
 };
