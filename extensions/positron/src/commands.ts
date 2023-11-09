bc008b38419f36479ad374a9a64797d188f10d40
Taylor Steinberg <taylor.steinberg@posit.co>
2023-11-09 11:00:19 -0500

    Adds dynamic port acquisition (#337)

---
+++ extensions/positron/src/commands.ts
@@ -0,0 +1,7 @@
+const executable: string = "connect-client";
+
+export type Command = string;
+
+export const create = (port: number, subcommand: string = "publish-ui"): Command => {
+    return `${executable} ${subcommand} --listen=127.0.0.1:${port} test/sample-content/fastapi-simple --skip-browser-session-auth`;
+};
