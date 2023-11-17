bb8159a67bbccab62992db3314cf0adc1f652ad1
Bill Sager <whs@sager.org>
2023-11-17 15:41:37 -0800

    Update main App to have toolbar w/ menu and logo

---
+++ web/src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<!-- Copyright (C) 2023 by Posit Software, PBC. -->
+
+<template>
+  <q-header
+    elevated
+    class="my-class"
+  >
+    <q-toolbar class="max-width-md q-pa-auto">
+      <AppMenu />
+      <WhitePositLogo
+        width="70px"
+        height="30px"
+        class="posit-logo"
+        alt="Posit PBC Logo"
+      />
+      <q-toolbar-title class="q-pl-xs">
+        Publisher
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script setup lang="ts">
+
+import AppMenu from 'src/components/AppMenu.vue';
+import WhitePositLogo from 'src/components/icons/WhitePositLogo.vue';
+
+</script>
