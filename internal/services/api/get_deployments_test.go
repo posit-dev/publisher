762b018970de56db6afc6ce7b2092d0b7fd3d8a0
Michael Marchetti <mike@rstudio.com>
2023-11-28 11:01:42 -0500

    deployment API tests

---
+++ internal/services/api/get_deployments_test.go
@@ -0,0 +1,103 @@
+package api
+
+// Copyright (C) 2023 by Posit Software, PBC.
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/rstudio/connect-client/internal/accounts"
+	"github.com/rstudio/connect-client/internal/deployment"
+	"github.com/rstudio/connect-client/internal/logging"
+	"github.com/rstudio/connect-client/internal/util"
+	"github.com/rstudio/connect-client/internal/util/utiltest"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/suite"
+)
+
+type GetDeploymentsSuite struct {
+	utiltest.Suite
+	log logging.Logger
+	cwd util.Path
+}
+
+func TestGetDeploymentsSuite(t *testing.T) {
+	suite.Run(t, new(GetDeploymentsSuite))
+}
+
+func (s *GetDeploymentsSuite) SetupSuite() {
+	s.log = logging.New()
+}
+
+func (s *GetDeploymentsSuite) SetupTest() {
+	fs := afero.NewMemMapFs()
+	cwd, err := util.Getwd(fs)
+	s.Nil(err)
+	s.cwd = cwd
+	s.cwd.MkdirAll(0700)
+}
+
+func (s *GetDeploymentsSuite) TestGetDeployments() {
+	path := deployment.GetLatestDeploymentPath(s.cwd, "myTargetID")
+	d := deployment.New()
+	d.ServerType = accounts.ServerTypeConnect
+	err := d.WriteFile(path)
+	s.NoError(err)
+
+	h := GetDeploymentsHandlerFunc(s.cwd, s.log)
+
+	rec := httptest.NewRecorder()
+	req, err := http.NewRequest("GET", "/api/deployments", nil)
+	s.NoError(err)
+	h(rec, req)
+
+	s.Equal(http.StatusOK, rec.Result().StatusCode)
+	s.Equal("application/json", rec.Header().Get("content-type"))
+
+	res := []deploymentDTO{}
+	dec := json.NewDecoder(rec.Body)
+	dec.DisallowUnknownFields()
+	s.NoError(dec.Decode(&res))
+	s.Len(res, 1)
+	s.Equal("myTargetID", res[0].ID)
+	s.Equal(d, res[0].Deployment)
+	s.Equal("", res[0].Error)
+}
+
+func (s *GetDeploymentsSuite) TestGetDeploymentsError() {
+	path := deployment.GetLatestDeploymentPath(s.cwd, "target1")
+	d := deployment.New()
+	d.ServerType = accounts.ServerTypeConnect
+	err := d.WriteFile(path)
+	s.NoError(err)
+
+	path2 := deployment.GetLatestDeploymentPath(s.cwd, "target2")
+	err = path2.WriteFile([]byte(`foo = 1`), 0666)
+	s.NoError(err)
+
+	h := GetDeploymentsHandlerFunc(s.cwd, s.log)
+
+	rec := httptest.NewRecorder()
+	req, err := http.NewRequest("GET", "/api/deployments", nil)
+	s.NoError(err)
+	h(rec, req)
+
+	s.Equal(http.StatusOK, rec.Result().StatusCode)
+	s.Equal("application/json", rec.Header().Get("content-type"))
+
+	res := []deploymentDTO{}
+	dec := json.NewDecoder(rec.Body)
+	dec.DisallowUnknownFields()
+	s.NoError(dec.Decode(&res))
+	s.Len(res, 2)
+	s.Equal("target1", res[0].ID)
+	s.Equal(d, res[0].Deployment)
+	s.Equal("", res[0].Error)
+
+	var nilDeployment *deployment.Deployment
+	s.Equal("target2", res[1].ID)
+	s.Equal(nilDeployment, res[1].Deployment)
+	s.NotEqual("", res[1].Error)
+}
