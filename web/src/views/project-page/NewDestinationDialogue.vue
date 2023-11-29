c67f0f32baa0d3510352e6acfe67bb38286f4ee4
Jordan Jensen <jordan.jensen@rstudio.com>
2023-11-29 15:55:53 -0800

    Create basic NewDestinationDialog

---
+++ web/src/views/project-page/NewDestinationDialogue.vue
@@ -0,0 +1,18 @@
+<!-- Copyright (C) 2023 by Posit Software, PBC. -->
+
+<template>
+  <q-dialog
+    :model-value="modelValue"
+    @update:model-value="val => emits('update:modelValue', val)"
+  >
+    HERE IT IS
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  modelValue: { type: Boolean, required: true }
+});
+const emits = defineEmits(['update:modelValue']);
+
+</script>
