804cff391d34d630375edbe6035d413361e2a603
Jordan Jensen <jordan.jensen@rstudio.com>
2023-11-29 16:18:15 -0800

    Create basic form that resets in new dialog

--- web/src/views/project-page/NewDestinationDialogue.vue
+++ web/src/views/project-page/NewDestinationDialogue.vue
@@ -3,16 +3,75 @@
 <template>
   <q-dialog
     :model-value="modelValue"
+    @hide="hide"
     @update:model-value="val => emits('update:modelValue', val)"
   >
-    HERE IT IS
+    <q-card>
+      <q-card-section class="row items-center q-pb-none">
+        <div class="text-h6">
+          Add Destination
+        </div>
+        <q-space />
+        <q-btn
+          v-close-popup
+          icon="close"
+          flat
+          round
+          dense
+        />
+      </q-card-section>
+
+      <q-card-section>
+        <q-form
+          class="q-gutter-md"
+          @reset="resetForm"
+          @submit.prevent="addDestination"
+        >
+          <q-input
+            v-model="contentId"
+            label="Content ID"
+          />
+
+          <div>
+            <q-btn
+              type="reset"
+              label="Cancel"
+              @click="hide"
+            />
+            <q-btn
+              type="submit"
+              color="primary"
+              class="q-ml-sm"
+              label="Add"
+            />
+          </div>
+        </q-form>
+      </q-card-section>
+    </q-card>
   </q-dialog>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
 defineProps({
   modelValue: { type: Boolean, required: true }
 });
 const emits = defineEmits(['update:modelValue']);
+
+const contentId = ref<string>('');
+
+function resetForm() {
+  contentId.value = '';
+}
+
+function addDestination() {
+  console.log('Destination added');
+}
+
+function hide() {
+  resetForm();
+  emits('update:modelValue', false);
+}
 
 </script>
