e51c6ff10da9afa113da1144649b8e0a0be4a3ee
Jordan Jensen <jordan.jensen@rstudio.com>
2023-12-01 12:39:29 -0800

    Pass contentId to NewDestinationHeader

--- web/src/views/new-deployment-destination/NewDeploymentDestinationPage.vue
+++ web/src/views/new-deployment-destination/NewDeploymentDestinationPage.vue
@@ -3,6 +3,7 @@
 <template>
   <NewDestinationHeader
     :account-name="accountName"
+    :content-id="contentId"
     class="q-mt-md"
   />
 </template>
@@ -26,9 +27,9 @@ const accountName = computed(() => {
 const contentId = computed(() => {
   // route param can be either string | string[]
   if (Array.isArray(route.params.contentId)) {
-    return route.params.contentId[0];
+    return route.params.contentId[0] || undefined;
   }
-  return route.params.contentId;
+  return route.params.contentId || undefined;
 });
 
 </script>
