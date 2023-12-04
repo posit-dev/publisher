67c48238858f80b7ca14876921953e7820031f51
Jordan Jensen <jordan.jensen@rstudio.com>
2023-12-04 14:36:05 -0800

    Create config component for each section

---
+++ web/src/components/config/ConfigAccess.vue
@@ -0,0 +1,26 @@
+<!-- Copyright (C) 2023 by Posit Software, PBC. -->
+
+<template>
+  <ConfigSetting
+    v-if="access"
+    label="Access"
+  >
+    {{ access }}
+  </ConfigSetting>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+import { AccessConfig } from 'src/api';
+import ConfigSetting from 'src/components/config/ConfigSetting.vue';
+
+defineProps({
+  access: {
+    type: Object as PropType<AccessConfig>,
+    default: undefined,
+    required: false,
+  },
+});
+</script>
+
---
+++ web/src/components/config/ConfigEnvironment.vue
@@ -0,0 +1,26 @@
+<!-- Copyright (C) 2023 by Posit Software, PBC. -->
+
+<template>
+  <ConfigSetting
+    v-if="environment"
+    label="Environment"
+  >
+    {{ environment }}
+  </ConfigSetting>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+import { EnvironmentConfig } from 'src/api';
+import ConfigSetting from 'src/components/config/ConfigSetting.vue';
+
+defineProps({
+  environment: {
+    type: Object as PropType<EnvironmentConfig>,
+    default: undefined,
+    required: false,
+  },
+});
+</script>
+
