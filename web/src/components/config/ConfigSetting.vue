71f1676c7fe905fcfb44ce698ccd5365952608cf
Jordan Jensen <jordan.jensen@rstudio.com>
2023-12-04 14:34:31 -0800

    Create generic ConfigSetting component

---
+++ web/src/components/config/ConfigSetting.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <dt>{{ label }}</dt>
+    <dd>
+      <slot />
+    </dd>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+});
+</script>
