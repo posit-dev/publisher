2dc9f4d37df6d0db60fda241618116322f46b768
tdstein <taylor.steinberg@posit.co>
2023-12-06 15:29:20 -0500

    Moves platform resolution to individual script

--- scripts/archive.bash
+++ scripts/archive.bash
@@ -16,39 +16,8 @@ echo "Version: $version" 1>&2
 name=$(basename "$cmd")
 echo "Name: $name" 1>&2
 
-platforms=("$(go env GOHOSTOS)/$(go env GOHOSTARCH)")
-if [ "$CI" = "true" ]; then
-    platforms=(
-        "darwin/amd64"
-        "darwin/arm64"
-        # "dragonfly/amd64"
-        # "freebsd/386"
-        # "freebsd/amd64"
-        # "freebsd/arm"
-        # "linux/386"
-        "linux/amd64"
-        # "linux/arm"
-        "linux/arm64"
-        # "linux/ppc64"
-        # "linux/ppc64le"
-        # "linux/mips"
-        # "linux/mipsle"
-        # "linux/mips64"
-        # "linux/mips64le"
-        # "netbsd/386"
-        # "netbsd/amd64"
-        # "netbsd/arm"
-        # "openbsd/386"
-        # "openbsd/amd64"
-        # "openbsd/arm"
-        # "plan9/386"
-        # "plan9/amd64"
-        # "solaris/amd64"
-        # "windows/386"
-        "windows/amd64"
-    )
-fi
-
+platforms=()
+while IFS='' read -r line; do platforms+=("$line"); done < <(./scripts/get-platforms.bash)
 for platform in "${platforms[@]}"
 do
     echo "Archiving: $platform" 1>&2
