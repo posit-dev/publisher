09cf43fa747e49ff537e0b4322d74a454fdf77ce
Jordan Jensen <jordan.jensen@rstudio.com>
2023-12-07 13:10:41 -0800

    Add files API types and method to module

--- web/src/api/client.ts
+++ web/src/api/client.ts
@@ -5,11 +5,13 @@ import axios from 'axios';
 import { Accounts } from 'src/api/resources/Accounts';
 import { Deployments } from 'src/api/resources/Deployments';
 import { Configurations } from 'src/api/resources/Configurations';
+import { Files } from 'src/api/resources/Files';
 
 class PublishingClientApi {
   accounts: Accounts;
   configurations: Configurations;
   deployments: Deployments;
+  files: Files;
 
   constructor() {
     const client = axios.create({
@@ -20,6 +22,7 @@ class PublishingClientApi {
     this.accounts = new Accounts(client);
     this.configurations = new Configurations(client);
     this.deployments = new Deployments(client);
+    this.files = new Files(client);
   }
 }
 
