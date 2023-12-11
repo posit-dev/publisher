9cc58f0267445d54771259e6181ff24c95fe61e7
Michael Marchetti <mike@rstudio.com>
2023-12-11 16:58:26 -0500

    better error message when init fails content type detection

--- cmd/publisher/commands/init.go
+++ cmd/publisher/commands/init.go
@@ -3,6 +3,8 @@ package commands
 // Copyright (C) 2023 by Posit Software, PBC.
 
 import (
+	"fmt"
+
 	"github.com/rstudio/connect-client/internal/cli_types"
 	"github.com/rstudio/connect-client/internal/config"
 	"github.com/rstudio/connect-client/internal/initialize"
@@ -15,10 +17,19 @@ type InitCommand struct {
 	ConfigName string    `name:"config" short:"c" help:"Configuration name to create (in .posit/publish/)"`
 }
 
+const contentTypeDetectionFailed = "could not determine content type; edit the configuration file (%s) to set the 'type' to a valid deployment type and 'entrypoint' to the main entrypoint file"
+
 func (cmd *InitCommand) Run(args *cli_types.CommonArgs, ctx *cli_types.CLIContext) error {
 	if cmd.ConfigName == "" {
 		cmd.ConfigName = config.DefaultConfigName
 	}
-	_, err := initialize.Init(cmd.Path, cmd.ConfigName, cmd.Python, ctx.Logger)
-	return err
+	cfg, err := initialize.Init(cmd.Path, cmd.ConfigName, cmd.Python, ctx.Logger)
+	if err != nil {
+		return err
+	}
+	if cfg.Type == config.ContentTypeUnknown {
+		configPath := config.GetConfigPath(cmd.Path, cmd.ConfigName)
+		return fmt.Errorf(contentTypeDetectionFailed, configPath)
+	}
+	return nil
 }
