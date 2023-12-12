af75793487286f0a3546090fe9bb94e03bdc7656
Jordan Jensen <jordan.jensen@rstudio.com>
2023-12-11 18:19:40 -0800

    Create ConfigCard component

---
+++ web/src/views/project-page/ConfigCard.vue
@@ -0,0 +1,32 @@
+<!-- Copyright (C) 2023 by Posit Software, PBC. -->
+
+<template>
+  <PCard :title="config.configurationName">
+    <div
+      v-if="isConfigurationError(config)"
+      class="row items-center"
+    >
+      <q-icon
+        name="error"
+        size="1rem"
+      />
+      <p class="q-ml-sm">
+        {{ config.error }}
+      </p>
+    </div>
+  </pcard>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+import { Configuration, ConfigurationError, isConfigurationError } from 'src/api';
+import PCard from 'src/components/PCard.vue';
+
+defineProps({
+  config: {
+    type: Object as PropType<Configuration | ConfigurationError>,
+    required: true,
+  },
+});
+</script>
