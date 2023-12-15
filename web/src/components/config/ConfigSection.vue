2bb3f62d42058603d4a4ba936064c7de6917956e
Jordan Jensen <jordan.jensen@rstudio.com>
2023-12-14 16:50:32 -0800

    Create ConfigSection component

---
+++ web/src/components/config/ConfigSection.vue
@@ -0,0 +1,57 @@
+<!-- Copyright (C) 2023 by Posit Software, PBC. -->
+
+<template>
+  <div>
+    <h3 class="config-section-title">
+      {{ title }}
+    </h3>
+    <p
+      v-if="subtitle"
+      class="q-mt-xs"
+    >
+      {{ subtitle }}
+    </p>
+    <dl class="q-mt-sm q-pt-sm space-between-lg">
+      <slot />
+    </dl>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.config-section-title {
+    font-size: 1rem;
+    font-weight: 500;
+    line-height: 1.5;
+}
+
+dl {
+    border: 1px solid;
+    border-radius: 8px;
+    padding: 16px;
+}
+
+.body--light {
+    dl {
+        border-color: $grey-4;
+    }
+}
+.body--dark {
+    dl {
+        border-color: $grey-8;
+    }
+}
+</style>
