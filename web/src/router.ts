bd382859f1779a3945af324b5443c41dc321af9d
Jordan Jensen <jordan.jensen@rstudio.com>
2023-12-19 13:49:20 -0800

    Always scroll to the top on routing

--- web/src/router.ts
+++ web/src/router.ts
@@ -43,4 +43,8 @@ const routes = [
 export const router = createRouter({
   history: createWebHashHistory(),
   routes: routes,
+  scrollBehavior() {
+    // always scroll to top
+    return { top: 0 };
+  },
 });
