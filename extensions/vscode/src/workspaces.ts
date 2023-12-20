f70ad61699cbafae1dd093a3e54b7c057eb7e21e
tdstein <taylor.steinberg@posit.co>
2023-12-20 13:20:09 -0500

    fix: uses fspath when getting workspace path

--- extensions/vscode/src/workspaces.ts
+++ extensions/vscode/src/workspaces.ts
@@ -1,14 +1,5 @@
 import * as vscode from 'vscode';
-import { win32, posix } from 'path';
 
-
-export const path = (platform: string = process.platform): string | undefined => {
-    let path = vscode.workspace.workspaceFolders?.at(0)?.uri.path;
-    if (platform === 'win32') {
-        // replace '/' with '\'
-        path = path?.split(posix.sep).join(win32.sep);
-        // remove leading '\'
-        path = path?.slice(1);
-    }
-    return path;
+export const path = (): string | undefined => {
+    return vscode.workspace.workspaceFolders?.at(0)?.uri.fsPath;
 };
