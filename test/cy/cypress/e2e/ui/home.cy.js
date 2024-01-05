04a596356e223982414121f31278b175b7dfec2d
Kevin Gartland <kgartland@rstudio.com>
2024-01-05 14:29:39 -0500

    dark mode tests

--- test/cy/cypress/e2e/ui/home.cy.js
+++ test/cy/cypress/e2e/ui/home.cy.js
@@ -7,4 +7,29 @@ describe('Landing', () => {
   it('.should() - assert that <title> is correct', () => {
     cy.title().should('include', 'Posit Publisher');
   });
+  
+  it('dark mode menu works', () => {
+    // expand dark mode menu
+    cy.expandDarkMenu();
+    cy.get('body').should('have.css', 'background-color', 'rgb(250, 250, 250)');
+
+    // set dark mode on
+    cy.get('div[data-automation="dark-on"]')
+      .click();
+    cy.get('body').should('have.css', 'background-color', 'rgb(18, 18, 18)');
+
+    cy.expandDarkMenu();
+
+    // set dark mode off
+    cy.get('div[data-automation="dark-off"]')
+      .click();
+    cy.get('body').should('have.css', 'background-color', 'rgb(250, 250, 250)');
+
+    cy.expandDarkMenu();
+
+    // set dark mode auto
+    cy.get('div[data-automation="dark-auto"]')
+      .click();
+    cy.get('body').should('have.css', 'background-color', 'rgb(250, 250, 250)');
+  });
 });
