6848fc40e3077f72ee46a9110e7bd1eee8ccceb2
Kevin Gartland <kgartland@rstudio.com>
2024-01-05 15:26:11 -0500

    more home tests

--- test/cy/cypress.config.js
+++ test/cy/cypress.config.js
@@ -6,6 +6,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    viewportHeight: 1080,
+    viewportWidth: 1920,
     video: true
   },
 });
--- test/cy/cypress/e2e/ui/home.cy.js
+++ test/cy/cypress/e2e/ui/home.cy.js
@@ -8,7 +8,7 @@ describe('Landing', () => {
     cy.title().should('include', 'Posit Publisher');
   });
   
-  it('dark mode menu works', () => {
+  it('dark mode works', () => {
     // expand dark mode menu
     cy.expandDarkMenu();
     cy.get('body').should('have.css', 'background-color', 'rgb(250, 250, 250)');
@@ -19,17 +19,32 @@ describe('Landing', () => {
     cy.get('body').should('have.css', 'background-color', 'rgb(18, 18, 18)');
 
     cy.expandDarkMenu();
-
     // set dark mode off
     cy.get('div[data-automation="dark-off"]')
       .click();
     cy.get('body').should('have.css', 'background-color', 'rgb(250, 250, 250)');
 
     cy.expandDarkMenu();
-
     // set dark mode auto
     cy.get('div[data-automation="dark-auto"]')
       .click();
     cy.get('body').should('have.css', 'background-color', 'rgb(250, 250, 250)');
+  });
+
+it('configuration file is displayed', () => {
+  cy.get('div[data-automation="config-card"]')
+    .find('h3[class="card-title truncate"]')
+    .contains("default");
+  
+  cy.get('div[data-automation="config-card"]')
+    .find('p')
+    .contains(".posit/publish/default.toml");
+  });
+
+it('files are displayed', () => {
+  cy.get('div[data-automation="file-tree"]')
+    .should('contain', 'fastapi-simple')
+    .and('contain', 'requirements.txt')
+    .and('contain', 'simple.py');
   });
 });
