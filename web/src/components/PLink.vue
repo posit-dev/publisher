f7964d629d406f315d4cc0bf4b4b6daf2e4378b5
Jordan Jensen <jordan.jensen@rstudio.com>
2024-01-10 16:56:42 -0800

    Create PLink component for extending router link

---
+++ web/src/components/PLink.vue
@@ -0,0 +1,33 @@
+<!-- Copyright (C) 2023 by Posit Software, PBC. -->
+
+<template>
+  <span
+    v-if="vscodeApi"
+    class="no-outline cursor-pointer"
+    tabindex="0"
+    @click="navigate"
+    @keyup.enter="navigate"
+  >
+    <slot />
+  </span>
+  <RouterLink
+    v-else
+    v-bind="props"
+  >
+    <slot />
+  </RouterLink>
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterLinkProps, useRouter } from 'vue-router';
+
+import { vscodeApi } from 'src/vscode';
+
+const router = useRouter();
+
+const props = defineProps<RouterLinkProps>();
+
+function navigate() {
+  router.push(props.to);
+}
+</script>
