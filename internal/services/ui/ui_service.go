2688ac1f6497ef2940a19e37e6046884a9cc57c1
Michael Marchetti <mike@rstudio.com>
2024-01-12 17:16:34 -0500

    add endpoint to the router

--- internal/services/ui/ui_service.go
+++ internal/services/ui/ui_service.go
@@ -91,6 +91,10 @@ func RouterHandlerFunc(base util.Path, lister accounts.AccountList, log logging.
 	r.Handle(ToPath("deployments", "{name}"), api.GetDeploymentHandlerFunc(base, log)).
 		Methods(http.MethodGet)
 
+	// DELETE /api/deployments/$NAME
+	r.Handle(ToPath("deployments", "{name}"), api.DeleteDeploymentHandlerFunc(base, log)).
+		Methods(http.MethodDelete)
+
 	// POST /api/deployments
 	r.Handle(ToPath("deployments"), api.PostDeploymentsHandlerFunc(base, log, lister)).
 		Methods(http.MethodPost)
