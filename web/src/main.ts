37fd977bafa3455aa8eb9dd915a848deb5f5e05f
Jordan Jensen <jordan.jensen@rstudio.com>
2024-01-18 15:03:51 -0800

    Set quasar dark mode based on vscode theme
    Effects the initialization and watches for class changes.

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,7 +2,7 @@
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import { Dark, Dialog, Quasar } from 'quasar';
+import { Dark, Dialog, Quasar, QuasarUIConfiguration } from 'quasar';
 
 // Import Quasar Roboto Font
 import '@quasar/extras/roboto-font/roboto-font.css';
@@ -17,13 +17,39 @@ import './style.css';
 import App from './App.vue';
 import { router } from './router';
 import eventStream from './plugins/eventStream';
+import { vscode, getVscodeTheme, onVscodeThemeChange } from './vscode';
 
 const pinia = createPinia();
 const app = createApp(App);
 
+const quasarUiConfig: QuasarUIConfiguration = { dark: 'auto' };
+if (vscode) {
+  switch (getVscodeTheme()) {
+    case 'light':
+      quasarUiConfig.dark = false;
+      break;
+    case 'dark':
+      quasarUiConfig.dark = true;
+      break;
+  }
+
+  onVscodeThemeChange((theme) => {
+    switch (theme) {
+      case 'light':
+      case 'high-contrast-light':
+        Dark.set(false);
+        break;
+      case 'dark':
+      case 'high-contrast-dark':
+        Dark.set(true);
+        break;
+    }
+  });
+}
+
 app.use(router);
 app.use(pinia);
-app.use(Quasar, { plugins: { Dark, Dialog } });
+app.use(Quasar, { config: quasarUiConfig, plugins: { Dark, Dialog } });
 app.use(eventStream);
 
 app.mount('#app');
