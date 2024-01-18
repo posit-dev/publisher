f20b13461f7dae8a63690aa142fd3982d32a935c
Jordan Jensen <jordan.jensen@rstudio.com>
2024-01-18 15:03:28 -0800

    Add vscode theme getter and observer

--- web/src/vscode.ts
+++ web/src/vscode.ts
@@ -3,3 +3,47 @@
 export const vscode = typeof acquireVsCodeApi !== 'undefined' ?
   acquireVsCodeApi()
   : undefined;
+
+export type VscodeTheme =
+  'light' |
+  'dark' |
+  'high-contrast-dark' |
+  'high-contrast-light';
+
+const themeFromClassList = (classes: DOMTokenList): VscodeTheme | undefined => {
+  if (classes.contains('vscode-light')) {
+    return 'light';
+  } else if (classes.contains('vscode-dark')) {
+    return 'dark';
+  } else if (classes.contains('vscode-high-contrast')) {
+    return 'high-contrast-dark';
+  } else if (classes.contains('vscode-high-contrast-light')) {
+    return 'high-contrast-light';
+  }
+};
+
+export const getVscodeTheme = (): VscodeTheme | undefined => {
+  return themeFromClassList(document.body.classList);
+};
+
+export const onVscodeThemeChange = (callback: (theme: VscodeTheme | undefined) => void) => {
+  let lastTheme = getVscodeTheme();
+
+  const observer = new MutationObserver((mutationList) => {
+    for (const mutation of mutationList) {
+      if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+        if (mutation.target instanceof Element) {
+          const theme = themeFromClassList(mutation.target.classList);
+          if (lastTheme !== theme) {
+            callback(theme);
+            lastTheme = theme;
+            break;
+          }
+        }
+      }
+    }
+  });
+
+  observer.observe(document.body, { attributes: true });
+  return observer;
+};
