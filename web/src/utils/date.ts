b4804be09e9d2f6aaa5374636eb8404818a58302
Jordan Jensen <jordan.jensen@rstudio.com>
2024-01-31 14:07:09 -0800

    Remove formatDateString in favor of using date-fns

--- web/src/utils/date.ts
+++ web/src/utils/date.ts
@@ -1,27 +1,5 @@
 // Copyright (C) 2023 by Posit Software, PBC.
 
-// depending on includeTime param, produces:
-// includeTime = false: "Dec 20, 2023"
-// includeTime = true: "Dec 20, 2023 at 12:39 PM"
-export function formatDateString(
-  dateString: string,
-  { includeTime } = { includeTime: true },
-) {
-  const dateResult = new Date(`${dateString}`).toLocaleDateString('en-US', {
-    day: '2-digit',
-    month: 'short',
-    year: 'numeric',
-  });
-  if (!includeTime) {
-    return dateResult;
-  }
-  const timeResult = new Date(`${dateString}`).toLocaleTimeString('en-US', {
-    hour: 'numeric',
-    minute: 'numeric',
-  });
-  return `${dateResult} at ${timeResult}`;
-}
-
 export function sortByDateString(a: string, b: string) {
   return Date.parse(a) > Date.parse(b) ? -1 : 1;
 }
