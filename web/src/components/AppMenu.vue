c20af30019397f782bfc49707cba4840c305d8ec
Bill Sager <whs@sager.org>
2024-02-01 10:09:25 -0800

    Handle vscode needs within menu items

--- web/src/components/AppMenu.vue
+++ web/src/components/AppMenu.vue
@@ -13,17 +13,21 @@
         <q-list class="q-pa-sm">
           <q-item
             v-close-popup
-            :to="{ name: 'project' }"
+            :clickable="clickable"
+            :to="toProject"
             data-automation="nav-project"
             class="q-my-sm"
+            @click="onClickProject"
           >
             <q-item-section>Project View</q-item-section>
           </q-item>
           <q-item
             v-close-popup
-            :to="{ name: 'agentLog' }"
+            :clickable="clickable"
+            :to="toAgentLog"
             data-automation="nav-agentLog"
             class="q-my-sm"
+            @click="onClickAgentLog"
           >
             <q-item-section>View Agent Log</q-item-section>
           </q-item>
@@ -86,8 +90,43 @@
 <script setup lang="ts">
 import { useQuasar } from 'quasar';
 import { vscode } from 'src/vscode';
+import { router } from 'src/router';
+import { computed } from 'vue';
 
 const $q = useQuasar();
 
 const version = __VERSION__;
+
+const onClickProject = () => {
+  if (vscode) {
+    return router.push({ name: 'project' });
+  }
+  return undefined;
+};
+
+const toProject = computed(() => {
+  if (!vscode) {
+    return { name: 'project' };
+  }
+  return undefined;
+});
+
+const onClickAgentLog = () => {
+  if (vscode) {
+    return router.push({ name: 'agentLog' });
+  }
+  return undefined;
+};
+
+const toAgentLog = computed(() => {
+  if (!vscode) {
+    return { name: 'agentLog' };
+  }
+  return undefined;
+});
+
+const clickable = computed(() => {
+  return Boolean(vscode);
+});
+
 </script>
