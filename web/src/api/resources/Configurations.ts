2363f62e110dc3ec875e385dec50ea2fb010f0c9
Michael Marchetti <mike@rstudio.com>
2024-02-26 11:13:13 -0500

    align configuration APIs

--- web/src/api/resources/Configurations.ts
+++ web/src/api/resources/Configurations.ts
@@ -2,7 +2,11 @@
 
 import { AxiosInstance } from 'axios';
 
-import { Configuration, ConfigurationError } from 'src/api/types/configurations';
+import {
+  Configuration,
+  ConfigurationDetails,
+  ConfigurationError,
+} from '../types/configurations';
 
 export class Configurations {
   private client: AxiosInstance;
@@ -18,5 +22,32 @@ export class Configurations {
     return this.client.get<Array<Configuration | ConfigurationError>>(
       '/configurations',
     );
+  }
+
+  // Returns:
+  // 200 - success
+  // 400 - bad request
+  // 500 - internal server error
+  createOrUpdate(configName: string, cfg: ConfigurationDetails) {
+    const encodedName = encodeURIComponent(configName);
+    return this.client.put<Configuration>(`configurations/${encodedName}`, cfg);
+  }
+
+  // Returns:
+  // 204 - success (no response)
+  // 404 - not found
+  // 500 - internal server error
+  delete(configName: string) {
+    const encodedName = encodeURIComponent(configName);
+    return this.client.delete(`configurations/${encodedName}`);
+  }
+
+  // Inspect the project, returning all possible (detected) configurations
+  // Returns:
+  // 200 - success
+  // 400 - bad request
+  // 500 - internal server error
+  inspect() {
+    return this.client.post<ConfigurationDetails[]>('/inspect');
   }
 }
