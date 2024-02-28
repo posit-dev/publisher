8637b7f3d5a44f7c6f59e6643e3b7b018a175fa7
Jordan Jensen <jordan.jensen@rstudio.com>
2024-02-28 12:36:54 -0800

    Group logs view in stages POC

--- extensions/vscode/src/views/logs.ts
+++ extensions/vscode/src/views/logs.ts
@@ -4,19 +4,31 @@ import * as vscode from 'vscode';
 
 import { EventStream, EventStreamMessage, displayEventStreamMessage } from '../events';
 
+enum LogStageStatus {
+  notStarted,
+  inProgress,
+  completed,
+  failed
+}
+
+type LogStage = {
+  status: LogStageStatus,
+  events: EventStreamMessage[]
+};
+
 const viewName = 'posit.publisher.logs';
 
 /**
  * Tree data provider for the Logs view.
  */
-export class LogsTreeDataProvider implements vscode.TreeDataProvider<LogsTreeItem> {
-  private events: EventStreamMessage[] = [];
+export class LogsTreeDataProvider implements vscode.TreeDataProvider<LogsTreeStageItem | LogsTreeItem> {
+  private stages!: Map<string, LogStage>;
 
   /**
    * Event emitter for when the tree data of the Logs view changes.
    * @private
    */
-  private _onDidChangeTreeData: vscode.EventEmitter<LogsTreeItem | undefined> = new vscode.EventEmitter<LogsTreeItem | undefined>();
+  private _onDidChangeTreeData: vscode.EventEmitter<LogsTreeStageItem | LogsTreeItem | undefined> = new vscode.EventEmitter<LogsTreeStageItem | LogsTreeItem | undefined>();
 
   /**
    * Creates an instance of LogsTreeDataProvider.
@@ -24,23 +36,62 @@ export class LogsTreeDataProvider implements vscode.TreeDataProvider<LogsTreeIte
    * @param {EventStream} stream - The event stream to listen to.
    */
   constructor(stream: EventStream) {
-    stream.on('message', (msg: EventStreamMessage) => {
-      if (msg.data.level !== 'DEBUG' && msg.type !== 'agent/log') {
-        this.events.push(msg);
-        this.refresh();
-      }
-    });
+    // Initialize the stages map
+    this.resetStages();
 
+    // Register all of the events this view cares about
+    this.registerEvents(stream);
+  };
+
+  private resetStages() {
+    this.stages = new Map([
+      ['publish/checkCapabilities', { status: LogStageStatus.notStarted, events: [] }]
+    ]);
+  }
+
+  private registerEvents(stream: EventStream) {
     // Reset events when a new publish starts
     stream.register('publish/start', (_: EventStreamMessage) => {
-      this.events = [];
+      this.resetStages();
     });
-  };
+
+    stream.register('publish/checkCapabilities/start', (_: EventStreamMessage) => {
+      const stage = this.stages.get('publish/checkCapabilities');
+      if (stage) {
+        stage.status = LogStageStatus.inProgress;
+      }
+      this.refresh();
+    });
+
+    stream.register('publish/checkCapabilities/log', (msg: EventStreamMessage) => {
+      const stage = this.stages.get('publish/checkCapabilities');
+      if (stage && msg.data.level !== 'DEBUG') {
+        stage.events.push(msg);
+      }
+      this.refresh();
+    });
+
+    stream.register('publish/checkCapabilities/success', (_: EventStreamMessage) => {
+      const stage = this.stages.get('publish/checkCapabilities');
+      if (stage) {
+        stage.status = LogStageStatus.completed;
+      }
+      this.refresh();
+    });
+
+    stream.register('publish/checkCapabilities/failure', (_: EventStreamMessage) => {
+      const stage = this.stages.get('publish/checkCapabilities');
+      if (stage) {
+        stage.status = LogStageStatus.failed;
+      }
+      this.refresh();
+    });
+  }
 
   /**
    * Get the event emitter for tree data changes.
    */
-  get onDidChangeTreeData(): vscode.Event<LogsTreeItem | undefined> {
+  get onDidChangeTreeData(): vscode.Event<LogsTreeStageItem | LogsTreeItem | undefined> {
     return this._onDidChangeTreeData.event;
   }
 
@@ -56,7 +107,7 @@ export class LogsTreeDataProvider implements vscode.TreeDataProvider<LogsTreeIte
    * @param element The element for which to get the tree item.
    * @returns The tree item representing the element.
    */
-  getTreeItem(element: LogsTreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
+  getTreeItem(element: LogsTreeStageItem | LogsTreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
     return element;
   }
 
@@ -65,9 +116,18 @@ export class LogsTreeDataProvider implements vscode.TreeDataProvider<LogsTreeIte
    * @param _ The parent element.
    * @returns The child elements of the parent element.
    */
-  getChildren(_: LogsTreeItem | undefined): vscode.ProviderResult<LogsTreeItem[]> {
+  getChildren(element?: LogsTreeStageItem | LogsTreeItem): vscode.ProviderResult<Array<LogsTreeStageItem | LogsTreeItem>> {
     // Map the events array to LogsTreeItem instances and return them as children
-    return this.events.map((event) => new LogsTreeItem(event, vscode.TreeItemCollapsibleState.None));
+    if (element instanceof LogsTreeStageItem) {
+      const result = element.events.map(e => new LogsTreeItem(e));
+      return result;
+    }
+
+    const result: LogsTreeStageItem[] = [];
+    this.stages.forEach((stage: LogStage) => {
+      result.push(new LogsTreeStageItem(stage, vscode.TreeItemCollapsibleState.Collapsed));
+    });
+    return result;
   }
 
   /**
@@ -86,11 +146,41 @@ export class LogsTreeDataProvider implements vscode.TreeDataProvider<LogsTreeIte
   }
 }
 
+export class LogsTreeStageItem extends vscode.TreeItem {
+  events: EventStreamMessage[] = [];
+
+  constructor(stage: LogStage, state: vscode.TreeItemCollapsibleState) {
+    super('Check Capabilities', state);
+    this.events = stage.events;
+    this.setIcon(stage.status);
+  }
+
+  setIcon(status: LogStageStatus) {
+    switch (status) {
+      case LogStageStatus.notStarted:
+        this.iconPath = new vscode.ThemeIcon('circle-outline');
+        break;
+      case LogStageStatus.inProgress:
+        this.iconPath = new vscode.ThemeIcon('loading~spin');
+        break;
+      case LogStageStatus.completed:
+        this.iconPath = new vscode.ThemeIcon('check');
+        break;
+      case LogStageStatus.failed:
+        this.iconPath = new vscode.ThemeIcon('error');
+        break;
+    }
+  }
+}
+
 /**
  * Represents a tree item for displaying logs in the tree view.
  */
 export class LogsTreeItem extends vscode.TreeItem {
-  constructor(msg: EventStreamMessage, state: vscode.TreeItemCollapsibleState = vscode.TreeItemCollapsibleState.None) {
+  constructor(
+    msg: EventStreamMessage,
+    state: vscode.TreeItemCollapsibleState = vscode.TreeItemCollapsibleState.None
+  ) {
     super(displayEventStreamMessage(msg), state);
     this.tooltip = JSON.stringify(msg);
   }
