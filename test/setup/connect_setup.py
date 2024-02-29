6712cbf05d511b546bb270dd8313de865fbc8819
Kevin Gartland <kgartland@rstudio.com>
2024-02-29 13:09:45 -0500

    use default small ec2 instance

--- test/setup/connect_setup.py
+++ test/setup/connect_setup.py
@@ -10,7 +10,7 @@ import os
 alias = "ubuntu22-publishing-client-2024.02"
 box_name = "connect-publishing-client"
 list_command = "fuzzbucket-client -j list"
-create_command = "fuzzbucket-client create -c -S 20 -t m5.2xlarge " + alias + " -n " + box_name
+create_command = "fuzzbucket-client create -c -S 20 " + alias + " -n " + box_name
 remove_command = "fuzzbucket-client rm " + box_name
 ssh_options = "-i fuzzbucket-ssh-key"
 
@@ -57,7 +57,7 @@ def get_ip(box_name):
 # check if fuzzbucket is up and taking requests
 def connect_ready(box_name, max_attempts, interval):
     connect_box=get_ip(box_name)
-    time.sleep(30)
+    time.sleep(90)
     update_config="fuzzbucket-client ssh " + box_name + " " + ssh_options + " sudo sed -i 's/CONNECT_IP/" + connect_box + "/g' /etc/rstudio-connect/rstudio-connect.gcfg"
     restart_connect = "fuzzbucket-client ssh " + box_name + " " + ssh_options + " sudo systemctl restart rstudio-connect"
     attempts = 0
