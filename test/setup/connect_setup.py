a59ca3081329b8786a0cf79d858602f8a9ce517e
Kevin Gartland <kgartland@rstudio.com>
2024-02-29 14:26:48 -0500

    use connect status

--- test/setup/connect_setup.py
+++ test/setup/connect_setup.py
@@ -57,9 +57,7 @@ def get_ip(box_name):
 # check if fuzzbucket is up and taking requests
 def connect_ready(box_name, max_attempts, interval):
     connect_box=get_ip(box_name)
-    time.sleep(90)
-    update_config="fuzzbucket-client ssh " + box_name + " " + ssh_options + " sudo sed -i 's/CONNECT_IP/" + connect_box + "/g' /etc/rstudio-connect/rstudio-connect.gcfg"
-    restart_connect = "fuzzbucket-client ssh " + box_name + " " + ssh_options + " sudo systemctl restart rstudio-connect"
+
     attempts = 0
     while attempts < max_attempts:
         try:
@@ -67,6 +65,8 @@ def connect_ready(box_name, max_attempts, interval):
             response = requests.get("http://"+connect_box+":3939/__ping__")
             if response.status_code == 200:
                 if connect_version != get_current_connect_version(get_ip(box_name), api_key):
+                    update_config="fuzzbucket-client ssh " + box_name + " " + ssh_options + " sudo sed -i 's/CONNECT_IP/" + connect_box + "/g' /etc/rstudio-connect/rstudio-connect.gcfg"
+                    restart_connect = "fuzzbucket-client ssh " + box_name + " " + ssh_options + " sudo systemctl restart rstudio-connect"
                     logging.info("Installing Connect on " + connect_box)
                     subprocess.check_output(install_connect, shell=True, text=True)
                     subprocess.check_output(update_config, shell=True, text=True)
