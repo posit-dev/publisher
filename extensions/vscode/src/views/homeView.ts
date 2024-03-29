e0a6e4ce73164e142b208abeeebb1bcef9baf17b
Bill Sager <whs@sager.org>
2024-03-29 15:12:45 -0700

    Shell for Home View

---
+++ extensions/vscode/src/views/homeView.ts
@@ -0,0 +1,127 @@
+// Copyright (C) 2024 by Posit Software, PBC.
+
+import {
+  Disposable,
+  Webview,
+  window,
+  Uri,
+  WebviewViewProvider,
+  WebviewView,
+  WebviewViewResolveContext,
+  CancellationToken,
+  ExtensionContext,
+} from "vscode";
+import { getUri } from "../utils/getUri";
+import { getNonce } from "../utils/getNonce";
+
+const viewName = "posit.publisher.homeView";
+
+export class HomeViewProvider implements WebviewViewProvider {
+  private _disposables: Disposable[] = [];
+
+  constructor(private readonly _extensionUri: Uri) {}
+
+  public resolveWebviewView(
+    webviewView: WebviewView,
+    _: WebviewViewResolveContext,
+    _token: CancellationToken,
+  ) {
+    // Allow scripts in the webview
+    webviewView.webview.options = {
+      // Enable JavaScript in the webview
+      enableScripts: true,
+      // Restrict the webview to only load resources from the `out` directory
+      localResourceRoots: [
+        Uri.joinPath(this._extensionUri, "out", "webviews", "homeView"),
+      ],
+    };
+
+    // Set the HTML content that will fill the webview view
+    webviewView.webview.html = this._getWebviewContent(
+      webviewView.webview,
+      this._extensionUri,
+    );
+  }
+  /**
+   * Defines and returns the HTML that should be rendered within the webview panel.
+   *
+   * @remarks This is also the place where references to the Vue webview build files
+   * are created and inserted into the webview HTML.
+   *
+   * @param webview A reference to the extension webview
+   * @param extensionUri The URI of the directory containing the extension
+   * @returns A template string literal containing the HTML that should be
+   * rendered within the webview panel
+   */
+  private _getWebviewContent(webview: Webview, extensionUri: Uri) {
+    // The CSS files from the Vue build output
+    const stylesUri = getUri(webview, extensionUri, [
+      "out",
+      "webviews",
+      "homeView",
+      "index.css",
+    ]);
+    // const codiconsUri = webview.asWebviewUri(Uri.joinPath(extensionUri, 'node_modules', '@vscode/codicons', 'dist', 'codicon.css'));
+    // The JS file from the Vue build output
+    const scriptUri = getUri(webview, extensionUri, [
+      "out",
+      "webviews",
+      "homeView",
+      "index.js",
+    ]);
+    // The codicon css (and related tff file) are needing to be loaded for icons
+    const codiconsUri = getUri(webview, extensionUri, [
+      "out",
+      "webviews",
+      "homeView",
+      "codicon.css",
+    ]);
+
+    const nonce = getNonce();
+
+    // Tip: Install the es6-string-html VS Code extension to enable code highlighting below
+    return /*html*/ `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta http-equiv="Content-Security-Policy"
+            content="default-src 'self' vscode-resource: https:;
+                    script-src vscode-resource: 'self' 'unsafe-inline' 'unsafe-eval' https:;
+                    style-src vscode-resource: 'self' 'unsafe-inline';
+                    img-src 'self' vscode-resource: data:"/>
+          <link rel="stylesheet" type="text/css" href="${stylesUri}">
+          <link rel="stylesheet" type="text/css" href="${codiconsUri}">
+          <title>Hello World</title>
+        </head>
+        <body>
+          <div id="app"></div>
+          <script type="module" nonce="${nonce}" src="${scriptUri}"></script>
+        </body>
+      </html>
+    `;
+  }
+
+  /**
+   * Cleans up and disposes of webview resources when view is disposed
+   */
+  public dispose() {
+    // Dispose of all disposables (i.e. commands) for the current webview panel
+    while (this._disposables.length) {
+      const disposable = this._disposables.pop();
+      if (disposable) {
+        disposable.dispose();
+      }
+    }
+  }
+
+  public register(context: ExtensionContext) {
+    const provider = window.registerWebviewViewProvider(viewName, this, {
+      webviewOptions: {
+        retainContextWhenHidden: true,
+      },
+    });
+    context.subscriptions.push(provider);
+  }
+}
