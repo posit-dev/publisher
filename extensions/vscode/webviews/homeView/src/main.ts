f68da9aa896ed5fea540ee67a7ef64ee33d99bc3
Bill Sager <whs@sager.org>
2024-04-02 17:18:33 -0700

    PR updates

--- extensions/vscode/webviews/homeView/src/main.ts
+++ extensions/vscode/webviews/homeView/src/main.ts
@@ -2,5 +2,24 @@
 
 import { createApp, inject } from "vue";
 import App from "./App.vue";
+import {
+  provideVSCodeDesignSystem,
+  vsCodeButton,
+  vsCodeDropdown,
+  vsCodeOption,
+  vsCodeProgressRing,
+  vsCodeDivider,
+} from "@vscode/webview-ui-toolkit";
+
+// In order to use the Webview UI Toolkit web components they
+// must be registered with the browser (i.e. webview) using the
+// syntax below.
+provideVSCodeDesignSystem().register(
+  vsCodeButton(),
+  vsCodeDropdown(),
+  vsCodeOption(),
+  vsCodeProgressRing(),
+  vsCodeDivider(),
+);
 
 createApp(App).mount("#app");
