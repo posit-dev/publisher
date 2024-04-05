fc7f179535c7f535113100a4dae9aa3a5c7170f1
Taylor Steinberg <taylor.steinberg@posit.co>
2024-04-05 14:36:55 -0400

    chore: removes dead code (#1301)

--- extensions/vscode/src/services.ts
+++ extensions/vscode/src/services.ts
@@ -3,20 +3,17 @@
 import * as vscode from "vscode";
 
 import { HOST } from ".";
-import { Panel } from "./panels";
 import { Server } from "./servers";
 import { useApi } from "./api";
 
 export class Service implements vscode.Disposable {
   private context: vscode.ExtensionContext;
-  private panel: Panel;
   private server: Server;
   private agentURL: string;
 
   constructor(context: vscode.ExtensionContext, port: number) {
     this.context = context;
     this.agentURL = `http://${HOST}:${port}/api`;
-    this.panel = new Panel(this.agentURL);
     this.server = new Server(port);
     useApi().setBaseUrl(this.agentURL);
   }
@@ -29,20 +26,12 @@ export class Service implements vscode.Disposable {
     return this.server.isUp();
   };
 
-  open = async () => {
-    // re-run the start sequence in case the server has stopped.
-    await this.server.start(this.context);
-    this.panel.show(this.context);
-  };
-
   stop = async () => {
     await this.server.stop();
-    this.panel.dispose();
     this.server.dispose();
   };
 
   dispose() {
-    this.panel.dispose();
     this.server.dispose();
   }
 }
