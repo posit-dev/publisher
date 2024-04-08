902f140a06dc2d00a234add2ef8a80ac5f4dc772
Jordan Jensen <jordan.jensen@rstudio.com>
2024-04-08 11:46:57 -0700

    Make PSelect work with toKey objects

--- extensions/vscode/webviews/homeView/src/components/PSelect.vue
+++ extensions/vscode/webviews/homeView/src/components/PSelect.vue
@@ -14,22 +14,39 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
 
-const model = defineModel<string>({ required: true });
+export type hasToKey = {
+  toKey: () => string;
+};
+
+export type SelectOption = string | hasToKey;
+
+const selectOptionToKey = (o: SelectOption): string => {
+  switch (typeof o) {
+    case "string":
+      return o;
+    case "object":
+      return o.toKey();
+  }
+};
+
+const model = defineModel<SelectOption>({ required: true });
 
 const props = defineProps<{
-  options: string[];
+  options: SelectOption[];
 }>();
 
-const _selection = ref<string>(model.value);
+const _selection = ref<string>(selectOptionToKey(model.value));
 
 const onInnerSelectionChange = (event: Event) => {
   const el = event.target as HTMLSelectElement;
   _selection.value = el.value;
 
-  model.value = props.options.find((option) => option === el.value) || "";
+  model.value =
+    props.options.find((option) => selectOptionToKey(option) === el.value) ||
+    "";
 };
 
-const stringifiedOptions = computed(() => {
-  return props.options.map((option) => option);
+const stringifiedOptions = computed((): string[] => {
+  return props.options.map((option) => selectOptionToKey(option));
 });
 </script>
