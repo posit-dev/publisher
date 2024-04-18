65a36cffccfc2aa9dfa5d6cda73f6b5094540552
Jordan Jensen <jordan.jensen@rstudio.com>
2024-04-18 15:11:42 -0700

    Add subtitle prop to TreeSection

--- extensions/vscode/webviews/homeView/src/components/TreeSection.vue
+++ extensions/vscode/webviews/homeView/src/components/TreeSection.vue
@@ -12,6 +12,7 @@
           :class="expanded ? 'codicon-chevron-down' : 'codicon-chevron-right'"
         />
         <h3 class="title">{{ title }}</h3>
+        <p v-if="subtitle">{{ subtitle }}</p>
       </div>
       <div v-if="actions" class="actions">
         <div class="monaco-toolbar">
@@ -58,6 +59,7 @@ const expanded = defineModel("expanded", { required: false, default: false });
 
 defineProps<{
   title: string;
+  subtitle?: string;
   actions?: TreeAction[];
 }>();
 
