538c95f6abb7a8f16517c0b1db2283ab9061f4a5
Kevin Gartland <kevin.gartland@posit.co>
2024-04-18 16:56:15 -0400

    add deployment

--- test/vscode-ui/test/specs/extension.spec.ts
+++ test/vscode-ui/test/specs/extension.spec.ts
@@ -28,12 +28,12 @@ describe("VS Code Extension UI Test", () => {
     await init.click();
 
     // name configuration and save
-    const actionbar = await browser.$("#quickInput_message");
-    await expect(actionbar).toHaveText(
-      "Configuration name (Press 'Enter' to confirm or 'Escape' to cancel)",
-    );
-    await browser.keys("blue");
-    await browser.keys([Key.Enter]);
+    // const actionbar = await browser.$("#quickInput_message");
+    // await expect(actionbar).toHaveText(
+    //   "Configuration name (Press 'Enter' to confirm or 'Escape' to cancel)",
+    // );
+    // await browser.keys("blue");
+    // await browser.keys([Key.Enter]);
 
     // click advanced mode
     const advancedMode = await browser.$("aria/Show Advanced Mode");
@@ -59,19 +59,64 @@ describe("VS Code Extension UI Test", () => {
     // await expect(newConfig).toHaveText("New Configuration");
     // await newConfig.click();
 
-    const defaultTextElement = await browser
+    const config = await browser
       .$(".monaco-pane-view")
       .$$(".split-view-view")[1]
       .$(".pane")
       .$(".pane-body");
-    await expect(defaultTextElement).toHaveText("blue");
+    await expect(config).toHaveText("default");
 
     // confirm config file is created
     const configFile = path.join(
       __dirname,
-      "../../../sample-content/fastapi-simple/.posit/publish/blue.toml",
+      "../../../sample-content/fastapi-simple/.posit/publish/default.toml",
     );
     expect(fs.existsSync(configFile)).toBeTruthy();
+
+    const deployments = await browser
+      .$(".monaco-pane-view")
+      .$$(".split-view-view")[3]
+      .$(".pane")
+      .$(".pane-body");
+    await expect(deployments).toHaveText("Untitled-1");
+
+    // confirm config file is created
+    const deployFile = path.join(
+      __dirname,
+      "../../../sample-content/fastapi-simple/.posit/publish/deployments/Untitled-1.toml",
+    );
+    expect(fs.existsSync(deployFile)).toBeTruthy();
+
+    // add deployment
+    // const newDeploy = await browser
+    // .$(".monaco-pane-view")
+    // .$$(".split-view-view")[3]
+    // .$(".pane")
+    // .$(".pane-header")
+    // .$(".monaco-toolbar")
+    // .$("codicon-add");
+    // await newDeploy.click();
+    // await browser.keys([Key.Enter]);
+    deployments.click();
+    const newDeploy = await browser.$("aria/Initiate New Deployment");
+    newDeploy.click();
+
+    // name configuration and save
+    // const actionbar = await browser.$("#quickInput_message");
+    // await expect(actionbar).toHaveText(
+    //   "Configuration name (Press 'Enter' to confirm or 'Escape' to cancel)",
+    // );
+    // await browser.keys("blue");
+    // await browser.keys([Key.Enter]);
+    const actionbar = await browser.$("#quickInput_message");
+    await expect(actionbar).toHaveText(
+      "Choose a unique name for the deployment (Press 'Enter' to confirm or 'Escape' to cancel)",
+    );
+    await browser.keys([Key.Enter]);
+    await browser.keys([Key.ArrowDown]);
+    await browser.keys([Key.Enter]);
+
+    await expect(deployments).toHaveText(expect.stringContaining("Untitled-2"));
 
     // cleanup
     after(async () => {
