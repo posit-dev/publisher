26587b0c9e8adddf2feee02764c922ee85ed58ec
Jordan Jensen <jordan.jensen@rstudio.com>
2024-04-19 14:48:37 -0700

    Simplify TreeItem

--- extensions/vscode/webviews/homeView/src/components/TreeItem.vue
+++ extensions/vscode/webviews/homeView/src/components/TreeItem.vue
@@ -1,35 +1,14 @@
 <template>
-  <div class="monaco-list-row">
-    <div class="monaco-tl-row">
-      <div class="resource">
-        <div class="name">
-          <div
-            class="monaco-icon-label file-icon .vscode-name-dir-icon settings.json-name-file-icon name-file-icon json-ext-file-icon ext-file-icon jsonc-lang-file-icon monaco-decoration-itemBadge-kkyaq5 monaco-decoration-iconBadge-kkyaq5"
-            aria-label="~/development/publishing-client/extensions/vscode/.vscode/settings.json • Modified"
-          >
-            <div class="monaco-icon-label-container">
-              <span class="monaco-icon-name-container">
-                <a class="label-name">
-                  <span class="monaco-highlighted-label">{{ title }}</span>
-                </a>
-              </span>
-              <span
-                v-if="description"
-                class="monaco-icon-description-container"
-              >
-                <span class="label-description">
-                  <span class="monaco-highlighted-label">
-                    {{ description }}
-                  </span>
-                </span>
-              </span>
-            </div>
-            <div v-if="actions" class="actions">
-              <ActionToolbar :title="title" :actions="actions" />
-            </div>
-          </div>
-        </div>
-        <div class="decoration-icon" title="" style="display: none"></div>
+  <div class="tree-item">
+    <div class="tree-item-container">
+      <div class="tree-item-label-container">
+        <span class="tree-item-title">{{ title }}</span>
+        <span v-if="description" class="tree-item-description">
+          {{ description }}
+        </span>
+      </div>
+      <div v-if="actions" class="actions">
+        <ActionToolbar :title="title" :actions="actions" />
       </div>
     </div>
   </div>
@@ -46,84 +25,56 @@ defineProps<{
 </script>
 
 <style lang="scss" scoped>
-.monaco-list-row {
+.tree-item {
   color: var(--vscode-foreground);
   cursor: pointer;
   touch-action: none;
-  overflow: hidden;
 
-  .monaco-tl-row {
+  .tree-item-container {
     display: flex;
-    align-items: center;
-    position: relative;
     overflow: hidden;
+    padding-left: 16px;
+    padding-right: 12px;
 
-    .resource {
-      display: flex;
+    .tree-item-label-container {
       flex: 1;
+      min-width: 0;
       overflow: hidden;
-      padding-left: 16px;
-      padding-right: 12px;
+      text-overflow: ellipsis;
 
-      .name {
-        flex: 1;
-        overflow: hidden;
-
-        .monaco-icon-label {
-          display: flex;
-          text-overflow: ellipsis;
-          overflow: hidden;
-
-          .monaco-icon-label-container {
-            flex: 1;
-            min-width: 0;
-            overflow: hidden;
-            text-overflow: ellipsis;
-
-            .monaco-icon-name-container {
-              line-height: 22px;
-
-              .label-name {
-                color: inherit;
-                white-space: pre;
-              }
-            }
-
-            .monaco-icon-description-container {
-              line-height: 22px;
-
-              .label-description {
-                font-size: 0.9em;
-                margin-left: 0.5em;
-                opacity: 0.7;
-                white-space: pre;
-              }
-            }
-          }
-        }
+      .tree-item-title {
+        line-height: 22px;
+        color: inherit;
+        white-space: pre;
       }
+
+      .tree-item-description {
+        line-height: 22px;
+        font-size: 0.9em;
+        margin-left: 0.5em;
+        opacity: 0.7;
+        white-space: pre;
+      }
+    }
+
+    .actions {
+      display: none;
+      max-width: fit-content;
+      flex-grow: 100;
     }
   }
 
   &:hover {
-    color: var(--vscode-list-hoverForeground);
+    color: var(--vscode-list-hoverForeground, var(--vscode-foreground));
     background-color: var(--vscode-list-hoverBackground);
 
-    .label-name {
-      color: var(--vscode-list-hoverForeground);
+    .tree-item-title {
+      color: var(--vscode-list-hoverForeground, var(--vscode-foreground));
     }
 
-    .label-description {
-      color: var(--vscode-list-hoverForeground);
+    .tree-item-description {
+      color: var(--vscode-list-hoverForeground, var(--vscode-foreground));
     }
-  }
-}
-
-.monaco-list-row {
-  .actions {
-    display: none;
-    max-width: fit-content;
-    flex-grow: 100;
   }
 
   &:hover .actions,
