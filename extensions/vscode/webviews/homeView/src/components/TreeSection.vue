9b17cc0e420a6ac4ab08bcf3e4520aea0240f71a
Jordan Jensen <jordan.jensen@rstudio.com>
2024-04-19 10:30:52 -0700

    Break out ActionToolbar component from Tree

--- extensions/vscode/webviews/homeView/src/components/TreeSection.vue
+++ extensions/vscode/webviews/homeView/src/components/TreeSection.vue
@@ -20,31 +20,7 @@
         </span>
       </div>
       <div v-if="actions" class="actions">
-        <div class="monaco-toolbar">
-          <div class="monaco-action-bar">
-            <ul
-              class="actions-container"
-              role="toolbar"
-              :aria-label="`${title} actions`"
-            >
-              <li
-                v-for="action in actions"
-                class="action-item menu-entry"
-                role="presentation"
-              >
-                <a
-                  class="action-label codicon"
-                  :class="action.codicon"
-                  role="button"
-                  :aria-label="action.label"
-                  tabindex="0"
-                  @click="action.fn"
-                  @keydown.enter="action.fn"
-                ></a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <ActionToolbar :title="title" :actions="actions" />
       </div>
     </div>
     <div v-if="expanded" class="pane-body">
@@ -54,18 +30,14 @@
 </template>
 
 <script setup lang="ts">
-export type TreeAction = {
-  label: string;
-  codicon: string;
-  fn: () => void;
-};
+import ActionToolbar, { ActionButton } from "./ActionToolbar.vue";
 
 const expanded = defineModel("expanded", { required: false, default: false });
 
 defineProps<{
   title: string;
   description?: string;
-  actions?: TreeAction[];
+  actions?: ActionButton[];
 }>();
 
 const toggleExpanded = () => {
@@ -78,57 +50,6 @@ const toggleExpanded = () => {
   .actions {
     display: none;
     margin-left: auto;
-
-    .monaco-toolbar {
-      height: 100%;
-
-      .monaco-action-bar {
-        white-space: nowrap;
-        height: 100%;
-
-        .actions-container {
-          align-items: center;
-          display: flex;
-          height: 100%;
-          margin: 0 auto;
-          padding: 0;
-          width: 100%;
-
-          .action-item {
-            display: block;
-            cursor: pointer;
-            position: relative;
-            z-index: 2;
-            margin-right: 4px;
-
-            .action-label {
-              border-radius: 5px;
-              padding: 2px;
-              font-size: 16px;
-              align-items: center;
-              display: flex;
-              height: 20px;
-              width: 20px;
-              color: var(--vscode-icon-foreground);
-
-              &:focus {
-                opacity: 1;
-                outline-color: var(--vscode-focusBorder);
-                outline-offset: -1px;
-                outline-style: solid;
-                outline-width: 1px;
-              }
-
-              &:hover {
-                background-color: var(--vscode-toolbar-hoverBackground);
-                outline: 1px dashed var(--vscode-toolbar-hoverOutline);
-                outline-offset: -1px;
-              }
-            }
-          }
-        }
-      }
-    }
   }
 
   &.expanded:hover .actions,
