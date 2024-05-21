5df0dbe26df33f5a7c8ddce9e37dfd08e27d14e2
Michael Marchetti <mike@rstudio.com>
2024-05-21 11:58:04 -0400

    tests for R scanning API

---
+++ internal/inspect/r_mock.go
@@ -0,0 +1,32 @@
+package inspect
+
+// Copyright (C) 2024 by Posit Software, PBC.
+
+import (
+	"github.com/rstudio/connect-client/internal/config"
+	"github.com/rstudio/connect-client/internal/util"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockRInspector struct {
+	mock.Mock
+}
+
+func NewMockRInspector() *MockRInspector {
+	return &MockRInspector{}
+}
+
+func (m *MockRInspector) InspectR() (*config.R, error) {
+	args := m.Called()
+	cfg := args.Get(0)
+	if cfg == nil {
+		return nil, args.Error(1)
+	} else {
+		return cfg.(*config.R), args.Error(1)
+	}
+}
+
+func (m *MockRInspector) CreateLockfile(lockfilePath util.AbsolutePath) error {
+	args := m.Called(lockfilePath)
+	return args.Error(0)
+}
