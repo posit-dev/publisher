3e4f4e58482632db3653f0476b4ba4ae96b51756
Kevin Gartland <kevin.gartland@posit.co>
2024-06-02 09:31:28 -0400

    use larger type

--- test/setup/connect_setup.py
+++ test/setup/connect_setup.py
@@ -10,7 +10,7 @@ import os
 alias = "ubuntu22-publishing-client-2024.05"
 box_name = "connect-publishing-client"
 list_command = "fuzzbucket-client -j list"
-create_command = "fuzzbucket-client create -c -S 30 " + alias + " -n " + box_name
+create_command = "fuzzbucket-client create -c -S 20 -t m5.2xlarge " + alias + " -n " + box_name
 remove_command = "fuzzbucket-client rm " + box_name
 ssh_options = "-i fuzzbucket-ssh-key"
 
