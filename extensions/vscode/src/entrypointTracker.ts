6df72c362896bf6ea8640b7d46550748ba71a2df
Jordan Jensen <jordan.jensen@rstudio.com>
2024-06-26 15:36:39 -0700

    Add on save updating to DocumentTracker

--- extensions/vscode/src/entrypointTracker.ts
+++ extensions/vscode/src/entrypointTracker.ts
@@ -6,6 +6,7 @@ import {
   window,
   workspace,
 } from "vscode";
+import { isActiveDocument } from "src/utils/files";
 
 const ACTIVE_FILE_ENTRYPOINT_CONTEXT = "posit.publish.activeFileEntrypoint";
 
@@ -15,6 +16,8 @@ const ACTIVE_FILE_ENTRYPOINT_CONTEXT = "posit.publish.activeFileEntrypoint";
 export class TrackedEntrypointDocument {
   readonly document: TextDocument;
   private isEntrypoint: boolean;
+
+  private requiresUpdate: boolean = false;
 
   private constructor(document: TextDocument, isEntrypoint: boolean) {
     this.document = document;
@@ -28,15 +31,39 @@ export class TrackedEntrypointDocument {
   }
 
   /**
-   * Sets the file entrypoint context with this as the active file
+   * Sets the file entrypoint context with this as the active file.
+   * @param options Options for the activation
+   * @param options.forceUpdate Whether to force the entrypoint to update
    */
-  activate() {
+  async activate(options?: { forceUpdate?: boolean }) {
     // change based on if entrypoint
+    if (options?.forceUpdate) {
+      await this.update();
+    } else if (this.requiresUpdate) {
+      await this.update();
+    }
+
     commands.executeCommand(
       "setContext",
       ACTIVE_FILE_ENTRYPOINT_CONTEXT,
       this.isEntrypoint,
     );
+  }
+
+  /**
+   * Updates the entrypoint next time the document is activated.
+   */
+  updateNextActivate() {
+    this.requiresUpdate = true;
+  }
+
+  /**
+   * Updates whether or not the document is an entrypoint file.
+   */
+  private async update() {
+    // set entrypoint with API call
+    // workspace.asRelativePath(editor.document.uri),
+    this.isEntrypoint = !this.isEntrypoint;
   }
 }
 
@@ -55,6 +82,7 @@ export class DocumentTracker implements Disposable {
     this.disposable = Disposable.from(
       window.onDidChangeActiveTextEditor(this.onActiveTextEditorChanged, this),
       workspace.onDidCloseTextDocument(this.onTextDocumentClosed, this),
+      workspace.onDidSaveTextDocument(this.onTextDocumentSaved, this),
     );
 
     // activate the initial active file
@@ -69,12 +97,12 @@ export class DocumentTracker implements Disposable {
   /**
    * Starts tracking a document
    * @param document The document to track
-   * @returns The document passed
+   * @returns The TrackedEntrypointDocument created
    */
   async addDocument(document: TextDocument) {
     const entrypoint = await TrackedEntrypointDocument.create(document);
     this.documents.set(document, entrypoint);
-    return document;
+    return entrypoint;
   }
 
   /**
@@ -105,18 +133,44 @@ export class DocumentTracker implements Disposable {
     let tracked = this.documents.get(editor.document);
 
     if (tracked === undefined) {
-      await this.addDocument(editor.document);
-      tracked = this.documents.get(editor.document);
+      tracked = await this.addDocument(editor.document);
     }
 
-    tracked?.activate();
+    tracked.activate();
   }
 
   /**
    * Listener function for the closing of a text document.
+   * Stops the document from being tracked.
+   *
    * @param document The closed document
    */
   onTextDocumentClosed(document: TextDocument) {
     this.removeDocument(document);
+  }
+
+  /**
+   * Listener function for the saving of a text document.
+   * Triggers the document to update next time it is activated.
+   *
+   * @param document The saved document
+   */
+  async onTextDocumentSaved(document: TextDocument) {
+    const tracked = this.documents.get(document);
+
+    if (tracked) {
+      if (isActiveDocument(document)) {
+        tracked.activate({ forceUpdate: true });
+      } else {
+        tracked.updateNextActivate();
+      }
+      return;
+    }
+
+    // Track the untracked document
+    const newTracked = await this.addDocument(document);
+    if (isActiveDocument(document)) {
+      newTracked.activate();
+    }
   }
 }
