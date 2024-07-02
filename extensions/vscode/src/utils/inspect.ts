a467ef9ef3b0580b25af6d79aaa92f439e8a17cc
Jordan Jensen <jordan.jensen@rstudio.com>
2024-07-02 15:52:33 -0700

    Add hasKnownContentType util function

---
+++ extensions/vscode/src/utils/inspect.ts
@@ -0,0 +1,15 @@
+import { ConfigurationInspectionResult, ContentType } from "src/api";
+
+/**
+ * Determines if some of the inspections have a known content type.
+ *
+ * @param inspections The results of an /api/inspect request
+ * @returns boolean - if any of the inspections have a known content type
+ */
+export function hasKnownContentType(
+  inspections: ConfigurationInspectionResult[],
+) {
+  return inspections.some(
+    (inspection) => inspection.configuration.type !== ContentType.UNKNOWN,
+  );
+}
