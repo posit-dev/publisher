22bdc75a8b7e4a51c758e520497e485dd0456dd3
Jordan Jensen <jordan.jensen@rstudio.com>
2024-07-01 17:15:12 -0700

    Add dir param support to ContentRecords resource

--- extensions/vscode/src/api/resources/ContentRecords.ts
+++ extensions/vscode/src/api/resources/ContentRecords.ts
@@ -18,17 +18,21 @@ export class ContentRecords {
   // Returns:
   // 200 - success
   // 500 - internal server error
-  getAll() {
-    return this.client.get<Array<AllContentRecordTypes>>("/deployments");
+  getAll(params?: { dir?: string }) {
+    return this.client.get<Array<AllContentRecordTypes>>("/deployments", {
+      params,
+    });
   }
 
   // Returns:
   // 200 - success
   // 404 - not found
   // 500 - internal server error
-  get(id: string) {
+  get(id: string, params?: { dir?: string }) {
     const encodedId = encodeURIComponent(id);
-    return this.client.get<AllContentRecordTypes>(`deployments/${encodedId}`);
+    return this.client.get<AllContentRecordTypes>(`deployments/${encodedId}`, {
+      params,
+    });
   }
 
   // Returns:
@@ -37,13 +41,18 @@ export class ContentRecords {
   // 409 - conflict
   // 500 - internal server error
   // Errors returned through event stream
-  createNew(accountName?: string, configName?: string, saveName?: string) {
+  createNew(
+    accountName?: string,
+    configName?: string,
+    saveName?: string,
+    params?: { dir?: string },
+  ) {
     const data = {
       account: accountName,
       config: configName,
       saveName,
     };
-    return this.client.post<PreContentRecord>("/deployments", data);
+    return this.client.post<PreContentRecord>("/deployments", data, { params });
   }
 
   // Returns:
@@ -55,15 +64,17 @@ export class ContentRecords {
     targetName: string,
     accountName: string,
     configName: string = "default",
+    params?: { dir?: string },
   ) {
-    const params = {
+    const data = {
       account: accountName,
       config: configName,
     };
     const encodedTarget = encodeURIComponent(targetName);
     return this.client.post<{ localId: string }>(
       `deployments/${encodedTarget}`,
-      params,
+      data,
+      { params },
     );
   }
 
@@ -71,19 +82,23 @@ export class ContentRecords {
   // 204 - no content
   // 404 - not found
   // 500 - internal server error
-  delete(saveName: string) {
+  delete(saveName: string, params?: { dir?: string }) {
     const encodedSaveName = encodeURIComponent(saveName);
-    return this.client.delete(`deployments/${encodedSaveName}`);
+    return this.client.delete(`deployments/${encodedSaveName}`, { params });
   }
 
   // Returns:
   // 204 - no content
   // 404 - contentRecord or config file not found
   // 500 - internal server error
-  patch(deploymentName: string, configName: string) {
+  patch(deploymentName: string, configName: string, params?: { dir?: string }) {
     const encodedName = encodeURIComponent(deploymentName);
-    return this.client.patch<ContentRecord>(`deployments/${encodedName}`, {
-      configurationName: configName,
-    });
+    return this.client.patch<ContentRecord>(
+      `deployments/${encodedName}`,
+      {
+        configurationName: configName,
+      },
+      { params },
+    );
   }
 }
