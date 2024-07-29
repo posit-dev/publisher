8eba5583e0916fd497dec68f53d32c292aca3c4b
Jordan Jensen <jordan.jensen@rstudio.com>
2024-07-29 14:01:48 -0700

    Set timezone to UTC when running vitest

--- extensions/vscode/webviews/homeView/src/utils/date.test.ts
+++ extensions/vscode/webviews/homeView/src/utils/date.test.ts
@@ -9,11 +9,11 @@ import {
 
 describe("formatDateString", () => {
   test("defaults to include time", () => {
-    const result = formatDateString("2024-07-23T16:38:20-07:00");
-    expect(result).toBe("Jul 23, 2024 at 04:38 PM");
+    const result = formatDateString("2024-07-23T16:38:20-08:00");
+    expect(result).toBe("Jul 24, 2024 at 12:38 AM");
   });
 
-  test("formats with short month", () => {
+  test("includeTime false provides only date", () => {
     const result = formatDateString("2024-07-23T16:38:20-07:00", {
       includeTime: false,
     });
@@ -21,29 +21,29 @@ describe("formatDateString", () => {
   });
 
   test("includeTime true includes hour, minute, and period", () => {
-    const result = formatDateString("2024-07-23T16:38:20-07:00", {
+    const result = formatDateString("2024-07-23T16:38:20-06:00", {
       includeTime: true,
     });
-    expect(result).toBe("Jul 23, 2024 at 04:38 PM");
+    expect(result).toBe("Jul 23, 2024 at 10:38 PM");
   });
 });
 
 describe("formatTimeString", () => {
   test("defaults to not include seconds", () => {
-    const result = formatTimeString("2024-07-23T11:38:20-07:00");
-    expect(result).toBe("11:38 AM");
+    const result = formatTimeString("2024-07-23T11:38:20-05:00");
+    expect(result).toBe("04:38 PM");
   });
 
   test("includes seconds when includeSeconds is true", () => {
-    const result = formatTimeString("2024-07-23T16:38:20-07:00", {
+    const result = formatTimeString("2024-07-23T16:38:20-04:00", {
       includeSeconds: true,
     });
-    expect(result).toBe("04:38:20 PM");
+    expect(result).toBe("08:38:20 PM");
   });
 
   test("formats time with 2-digit hour and minute", () => {
-    const result = formatTimeString("2024-07-23T16:38:20-07:00");
-    expect(result).toBe("04:38 PM");
+    const result = formatTimeString("2024-07-23T16:38:20-03:00");
+    expect(result).toBe("07:38 PM");
   });
 });
 
