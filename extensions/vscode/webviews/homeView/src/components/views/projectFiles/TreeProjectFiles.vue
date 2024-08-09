3bae7d1c2070cedb5009412f88bbc5abcc6fcbf9
Jordan Jensen <jordan.jensen@rstudio.com>
2024-08-09 12:18:12 -0700

    Add actions to TreeProjectFiles view type

--- extensions/vscode/webviews/homeView/src/components/views/projectFiles/TreeProjectFiles.vue
+++ extensions/vscode/webviews/homeView/src/components/views/projectFiles/TreeProjectFiles.vue
@@ -9,6 +9,7 @@
         : includedFileTooltip(file)
     "
     :indentLevel="indentLevel"
+    :actions="fileActions(file)"
     v-on="{
       click: file.isFile ? () => openFile(file) : undefined,
     }"
@@ -27,8 +28,9 @@ import {
 } from "src/components/views/projectFiles/tooltips";
 import TreeProjectFiles from "src/components/views/projectFiles/TreeProjectFiles.vue";
 import { useHostConduitService } from "src/HostConduitService";
+import { ActionButton } from "src/components/ActionToolbar.vue";
 
-import { ContentRecordFile } from "../../../../../../src/api";
+import { ContentRecordFile, FileMatchSource } from "../../../../../../src/api";
 import { WebviewToHostMessageType } from "../../../../../../src/types/messages/webviewToHostMessages";
 
 interface Props {
@@ -47,5 +49,35 @@ const openFile = (file: ContentRecordFile) => {
     kind: WebviewToHostMessageType.VSCODE_OPEN_RELATIVE,
     content: { relativePath: file.id },
   });
+};
+
+const fileActions = (file: ContentRecordFile): ActionButton[] => {
+  let actions: ActionButton[] = [];
+
+  if (file.reason?.exclude && file.reason?.source != FileMatchSource.BUILT_IN) {
+    actions.push({
+      label: file.isFile ? "Include this file" : "Include this folder",
+      codicon: "codicon-diff-added",
+      fn: () =>
+        sendMsg({
+          kind: WebviewToHostMessageType.INCLUDE_FILE,
+          content: { path: file.id },
+        }),
+    });
+  }
+
+  if (!file.reason?.exclude) {
+    actions.push({
+      label: file.isFile ? "Exclude this file" : "Exclude this folder",
+      codicon: "codicon-diff-removed",
+      fn: () =>
+        sendMsg({
+          kind: WebviewToHostMessageType.EXCLUDE_FILE,
+          content: { path: file.id },
+        }),
+    });
+  }
+
+  return actions;
 };
 </script>
