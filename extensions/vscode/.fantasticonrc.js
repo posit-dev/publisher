5e67dc9d3a743fc2c4c7156c86b661c714137a2b
Kevin Gartland <kevin.gartland@posit.co>
2024-08-16 12:02:02 -0400

    revert fantasticon changes

--- extensions/vscode/.fantasticonrc.js
+++ extensions/vscode/.fantasticonrc.js
@@ -1,22 +1,12 @@
-const path = require("path");
-
-// fantasticon does not support windows paths, this fixes the input path for windows
-// https://github.com/tancredi/fantasticon/pull/510
-const fixWindowsPath = (p) =>
-  process.platform === "win32" ? p.replace(/\\/g, "/") : p;
-const inputDir = fixWindowsPath(path.join(__dirname, "assets", "icons"));
-const outputDir = fixWindowsPath(path.join(__dirname, "dist"));
-const codepoints = fixWindowsPath(
-  path.join(__dirname, "assets", "icons", "template", "mapping.json"),
-);
+const codepoints = require("./assets/icons/template/mapping.json");
 
 /** @type {import('@twbs/fantasticon').RunnerOptions} */
 const config = {
   name: "posit-publisher-icons",
   prefix: "posit-publisher-icons",
   codepoints: codepoints,
-  inputDir: inputDir,
-  outputDir: outputDir,
+  inputDir: "./assets/icons",
+  outputDir: "./dist",
   fontTypes: ["woff2"],
   normalize: true,
   assetTypes: ["html", "css", "json"],
