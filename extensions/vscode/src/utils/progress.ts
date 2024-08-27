7b7826cd1961cabb78714f83e11a2779735d4bc7
Jordan Jensen <jordan.jensen@rstudio.com>
2024-08-27 16:30:06 -0700

    Create showProgressPassthrough helper

--- extensions/vscode/src/utils/progress.ts
+++ extensions/vscode/src/utils/progress.ts
@@ -24,3 +24,27 @@ export async function showProgress(
     console.log(`Progress for "${title}" was displayed for ${duration}ms`);
   }
 }
+
+export async function showProgressPassthrough<T>(
+  title: string,
+  viewId: string,
+  until: () => Promise<T>,
+  trace = true,
+): Promise<T> {
+  const start = performance.now();
+
+  try {
+    return await window.withProgress(
+      {
+        title,
+        location: viewId ? { viewId } : ProgressLocation.Window,
+      },
+      until,
+    );
+  } finally {
+    if (trace) {
+      const duration = Math.round(Number(performance.now() - start));
+      console.log(`Progress for "${title}" was displayed for ${duration}ms`);
+    }
+  }
+}
