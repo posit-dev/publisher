5989ee7a4822878e71e54ea6c6a4983d7d95b014
Bill Sager <whs@sager.org>
2024-08-27 09:10:18 -0700

    decode 500 error's data string

--- extensions/vscode/src/api/client.ts
+++ extensions/vscode/src/api/client.ts
@@ -1,6 +1,6 @@
 // Copyright (C) 2023 by Posit Software, PBC.
 
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 import { Credentials } from "./resources/Credentials";
 import { ContentRecords } from "./resources/ContentRecords";
@@ -8,6 +8,7 @@ import { Configurations } from "./resources/Configurations";
 import { Files } from "./resources/Files";
 import { Packages } from "./resources/Packages";
 import { EntryPoints } from "./resources/Entrypoints";
+import * as Entities from "entities";
 
 class PublishingClientApi {
   private client;
@@ -29,15 +30,20 @@ class PublishingClientApi {
       return request;
     });
 
-    this.client.interceptors.response.use((response) => {
-      const timestamp = response.config.ts;
-      if (timestamp) {
-        const request = response.request;
-        const duration = Math.round(Number(performance.now() - timestamp));
-        console.log(`Request: ${request.path} took ${duration}ms`);
-      }
-      return response;
-    });
+    this.client.interceptors.response.use(
+      (response) => {
+        this.logDuration(response);
+        return response;
+      },
+      (error) => {
+        // Decode data returned for 500 errors
+        if (error.response.status === 500) {
+          error.response.data = Entities.decodeHTML5(error.response.data);
+        }
+        this.logDuration(error.response);
+        return Promise.reject(error);
+      },
+    );
     this.apiServiceIsUp = apiServiceIsUp;
 
     this.configurations = new Configurations(this.client);
@@ -46,6 +52,16 @@ class PublishingClientApi {
     this.files = new Files(this.client);
     this.packages = new Packages(this.client);
     this.entrypoints = new EntryPoints(this.client);
+  }
+
+  logDuration(response: AxiosResponse<any, any>) {
+    const timestamp = response.config.ts;
+    if (timestamp) {
+      const request = response.request;
+      const duration = Math.round(Number(performance.now() - timestamp));
+      console.log(`Request: ${request.path} took ${duration}ms`);
+    }
+    return response;
   }
 
   setBaseUrl(url: string) {
