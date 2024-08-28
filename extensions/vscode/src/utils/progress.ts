6d6796ecdf1e0747fc226dcda0494a5a01edf001
Jordan Jensen <jordan.jensen@rstudio.com>
2024-08-27 17:38:16 -0700

    Remove original showProgress

--- extensions/vscode/src/utils/progress.ts
+++ extensions/vscode/src/utils/progress.ts
@@ -2,29 +2,6 @@
 
 import { ProgressLocation, window } from "vscode";
 
-export async function showProgress(
-  title: string,
-  until: Promise<any>,
-  viewId: string,
-  trace = true,
-) {
-  const start = performance.now();
-  window.withProgress(
-    {
-      title,
-      location: viewId ? { viewId } : ProgressLocation.Window,
-    },
-    () => {
-      return until;
-    },
-  );
-  await until;
-  if (trace) {
-    const duration = Math.round(Number(performance.now() - start));
-    console.log(`Progress for "${title}" was displayed for ${duration}ms`);
-  }
-}
-
 export async function showProgressPassthrough<T>(
   title: string,
   viewId: string,
