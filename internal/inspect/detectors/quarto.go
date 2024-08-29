c590b8c6ab80b2a6961e8d2c80dc93d925899e93
Jordan Jensen <jordan.jensen@rstudio.com>
2024-08-28 17:07:30 -0700

    Preprend slash in config files for all types

--- internal/inspect/detectors/quarto.go
+++ internal/inspect/detectors/quarto.go
@@ -288,7 +288,7 @@ func (d *QuartoDetector) InferType(base util.AbsolutePath, entrypoint util.Relat
 			} else {
 				relPath = inputFile
 			}
-			cfg.Files = append(cfg.Files, relPath)
+			cfg.Files = append(cfg.Files, fmt.Sprint("/", relPath))
 		}
 		extraFiles := []string{"_quarto.yml", "_metadata.yaml"}
 		for _, filename := range extraFiles {
@@ -298,7 +298,7 @@ func (d *QuartoDetector) InferType(base util.AbsolutePath, entrypoint util.Relat
 				return nil, err
 			}
 			if exists {
-				cfg.Files = append(cfg.Files, filename)
+				cfg.Files = append(cfg.Files, fmt.Sprint("/", filename))
 			}
 		}
 		configs = append(configs, cfg)
