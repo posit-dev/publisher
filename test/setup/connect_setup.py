5e662190c5e3c4df19c23b66bcdc009566752dd4
Kevin Gartland <kevin.gartland@posit.co>
2024-09-16 12:26:34 -0400

    update fuzzbucket with python 3.12

--- test/setup/connect_setup.py
+++ test/setup/connect_setup.py
@@ -7,7 +7,7 @@ import logging
 import os
 
 # use the perftest fuzzbucket instance since it already has all the deps
-alias = "ubuntu22-publishing-client-2024.09"
+alias = "ubuntu22-publishing-client-2024.0916"
 box_name = "connect-publishing-client"
 list_command = "fuzzbucket-client -j list"
 create_command = "fuzzbucket-client create -c -S 20 -t m5.2xlarge " + alias + " -n " + box_name
