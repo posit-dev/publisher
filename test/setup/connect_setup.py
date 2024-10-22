08ef9d0d7436835b3445e9ba2da874330644581e
Kevin Gartland <kevin.gartland@posit.co>
2024-10-22 14:03:52 -0400

    separate ip script

--- test/setup/connect_setup.py
+++ test/setup/connect_setup.py
@@ -5,6 +5,7 @@ import requests
 import time
 import logging
 import os
+import sys
 
 # use the perftest fuzzbucket instance since it already has all the deps
 alias = "ubuntu22-publishing-client-2024.10"
@@ -32,7 +33,7 @@ def get_current_connect_version(connect_ip, api_key):
     response = requests.get(
         'http://' + connect_ip + ':3939/__api__/server_settings',
         headers={'Authorization': 'Key ' + api_key},
-        )
+        )   
     current_connect = response.json()['version']
     return current_connect
 
@@ -67,7 +68,6 @@ def connect_ready(box_name, max_attempts, interval):
                 if connect_version != get_current_connect_version(get_ip(box_name), api_key):
                     update_config="fuzzbucket-client ssh " + box_name + " " + ssh_options + " sudo sed -i 's/CONNECT_IP/" + connect_box + "/g' /etc/rstudio-connect/rstudio-connect.gcfg"
                     restart_connect = "fuzzbucket-client ssh " + box_name + " " + ssh_options + " sudo systemctl restart rstudio-connect"
-                    logging.info("Installing Connect on " + connect_box)
                     # run kill script on box
                     subprocess.check_output(check_lock, shell=True, text=True)
                     subprocess.check_output(install_connect, shell=True, text=True)
---
+++ test/setup/get_ip.py
@@ -0,0 +1,5 @@
+from connect_setup import get_ip
+
+box_name = "connect-publishing-client"
+
+connect_ip = get_ip(box_name)
