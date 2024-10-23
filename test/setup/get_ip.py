7ccba372c4d37e0ac15f4e1af6e1ea5372fb1db9
Kevin Gartland <kevin.gartland@posit.co>
2024-10-23 12:22:28 -0400

    simplify script

--- test/setup/get_ip.py
+++ test/setup/get_ip.py
@@ -1,5 +1,10 @@
-from connect_setup import get_ip
+import subprocess
+import json
 
+list_command = "fuzzbucket-client -j list"
 box_name = "connect-publishing-client"
 
-connect_ip = get_ip(box_name)
+output = subprocess.check_output(list_command, shell=True, text=True)
+boxes = json.loads(output)
+connect_ip = boxes["boxes"][box_name]["public_ip"]
+print("http://"+connect_ip+":3939")
