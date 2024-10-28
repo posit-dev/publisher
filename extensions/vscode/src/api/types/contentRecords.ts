09b3b97e049725d8fb42a24bde57ca63c7c6a334
Jordan Jensen <jordan.jensen@rstudio.com>
2024-10-28 14:33:52 -0700

    Add frontend types for environment API endpoint

--- extensions/vscode/src/api/types/contentRecords.ts
+++ extensions/vscode/src/api/types/contentRecords.ts
@@ -76,6 +76,8 @@ export type AllContentRecordTypes =
   | PreContentRecordWithConfig
   | ContentRecordError;
 
+export type Environment = Array<string>;
+
 export function isSuccessful(
   d: AllContentRecordTypes | undefined,
 ): boolean | undefined {
