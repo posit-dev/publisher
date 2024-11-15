a483c7b47247c7bf7bf2725734e23c7058717c38
Jordan Jensen <jordan.jensen@rstudio.com>
2024-11-15 12:22:25 -0800

    Add ignores to eslint config

--- extensions/vscode/eslint.config.mjs
+++ extensions/vscode/eslint.config.mjs
@@ -6,6 +6,7 @@ import eslintConfigPrettier from "eslint-config-prettier";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { ignores: ["out", "dist", "**/*.d.ts", "api"] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
