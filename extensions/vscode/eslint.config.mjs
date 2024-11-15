c31317884ba24745e2eb64aac09d778937d4fe7c
Jordan Jensen <jordan.jensen@rstudio.com>
2024-11-15 12:22:18 -0800

    Add eslint-config-prettier

--- extensions/vscode/eslint.config.mjs
+++ extensions/vscode/eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -8,4 +9,5 @@ export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
 ];
