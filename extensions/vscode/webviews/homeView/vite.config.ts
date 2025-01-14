e7c08b055beb5e91c69c3b0a436beed7933a65ba
Bill Sager <whs@sager.org>
2025-01-14 14:46:53 -0800

    reduction in precision

--- extensions/vscode/webviews/homeView/vite.config.ts
+++ extensions/vscode/webviews/homeView/vite.config.ts
@@ -41,9 +41,9 @@ export default defineConfig({
       enabled: true,
       thresholds: {
         functions: 30.13,
-        lines: 17,
+        lines: 17.0,
         branches: 44.82,
-        statements: 17,
+        statements: 17.0,
         autoUpdate: true,
       },
     },
