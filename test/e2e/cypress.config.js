bcfa1bffa052c09b0e2fc6dc9c8658b2e69d0fca
Marcos Navarro <m@marcosn.com>
2025-01-21 16:22:43 -0600

    Cypress e2e infra initial setup

---
+++ test/e2e/cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:8080",
+    supportFile: "support/index.js",
+    specPattern: "tests/**/*.cy.{js,jsx,ts,tsx}",
+    // eslint-disable-next-line no-unused-vars
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  env: {
+    BOOTSTRAP_ADMIN_API_KEY: "", // To be updated by Cypress when spinning up
+    BOOTSTRAP_SECRET_KEY: "bootstrap-secret.key", // To be updated by Cypress when spinning up
+    CONNECT_SERVER_URL: "http://localhost:3939",
+    CONNECT_MANAGER_URL: "http://localhost:4723",
+  },
+  chromeWebSecurity: false,
+});
---
+++ test/e2e/tests/deployments.cy.js
@@ -0,0 +1,72 @@
+describe("Deployments Section", () => {
+  beforeEach(() => {
+    cy.resetConnect();
+    cy.setAdminCredentials();
+    cy.visit("/");
+  });
+
+  it("Static Content Deployment", () => {
+    // Temporarily ignore uncaught exception due to a vscode worker being cancelled at some point.
+    cy.on("uncaught:exception", () => false);
+
+    // Open the entrypoint ahead of time for easier selection later.
+    cy.get(".explorer-viewlet").find('[aria-label="static"]').click();
+
+    cy.get(".explorer-viewlet")
+      .find('[aria-label="index.html"]')
+      .should("be.visible")
+      .dblclick();
+
+    cy.get(".tabs-container")
+      .find('[aria-label="index.html"]')
+      .should("be.visible");
+
+    cy.getPublisherSidebarIcon()
+      .should("be.visible", { timeout: 10000 })
+      .click();
+
+    cy.publisherWebview()
+      .findByTestId("select-deployment")
+      .then((dplyPicker) => {
+        Cypress.$(dplyPicker).trigger("click");
+      });
+
+    cy.get(".quick-input-widget").should("be.visible");
+
+    cy.get(".quick-input-titlebar").should("have.text", "Select Deployment");
+
+    cy.get(".quick-input-widget").type("{enter}");
+
+    cy.get(".quick-input-widget")
+      .find('[aria-label="static/index.html, Open Files"]')
+      .should("be.visible")
+      .click();
+
+    cy.get(".quick-input-widget")
+      .find(".quick-input-filter input")
+      .should("have.value", "static")
+      .type("{enter}");
+
+    cy.get(".quick-input-widget")
+      .find(
+        '[aria-label="admin-code-server, http://connect-publisher-e2e:3939"]',
+      )
+      .should("be.visible")
+      .click();
+
+    cy.publisherWebview()
+      .findByTestId("deploy-button")
+      .should("be.visible")
+      .then((dplyBtn) => {
+        Cypress.$(dplyBtn).trigger("click");
+      });
+
+    // Wait for deploying  message to finish
+    cy.get(".notifications-toasts")
+      .should("be.visible")
+      .findByText("Deploying your project: Starting to Deploy...")
+      .should("not.exist");
+
+    cy.findByText("Deployment was successful").should("be.visible");
+  });
+});
