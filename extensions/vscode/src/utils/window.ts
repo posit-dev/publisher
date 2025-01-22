f05347785ff56e51ee2f8bc02e8e3b71b0092e11
Jordan Jensen <jordan.jensen@rstudio.com>
2025-01-22 13:53:02 -0800

    Include period only when necessary in notification

--- extensions/vscode/src/utils/window.ts
+++ extensions/vscode/src/utils/window.ts
@@ -6,6 +6,11 @@ export function showErrorMessageWithTroubleshoot(
   message: string,
   ...items: string[]
 ) {
-  const msg = `${message} See [Troubleshooting docs](https://github.com/posit-dev/publisher/blob/main/docs/troubleshooting.md) for help.`;
+  let msg = message;
+  if (!message.endsWith(".")) {
+    msg += ".";
+  }
+  msg +=
+    " See [Troubleshooting docs](https://github.com/posit-dev/publisher/blob/main/docs/troubleshooting.md) for help.";
   return window.showErrorMessage(msg, ...items);
 }
