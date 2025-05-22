9a725a5be65c9ece1361cf4d0643c1d2edd5b982
Chris Tierney <chris.tierney@gmail.com>
2025-05-22 14:28:59 -0400

    release 1.16.0 (#2652)

--- install-publisher.bash
+++ install-publisher.bash
@@ -148,7 +148,7 @@ esac
 
 # version override, swap out latest with the latest and greatest
 if [[ $VERSION_TYPE == "release" && $VERSION == "latest" ]]; then
-  VERSION="1.14.0"
+  VERSION="1.16.0"
 fi
 
 # Variables
