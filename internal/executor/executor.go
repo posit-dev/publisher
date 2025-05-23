32f2ce1160030b52e70894cb979c0a8f373e84ed
Jonathan Keane <jkeane@gmail.com>
2025-05-23 18:02:25 -0500

    clean up RunScript to use RunCommand

--- internal/executor/executor.go
+++ internal/executor/executor.go
@@ -43,55 +43,27 @@ func (e *defaultExecutor) RunCommand(executable string, args []string, cwd util.
 	return stdout.Bytes(), stderrBuf.Bytes(), err
 }
 
-// createTempRScript creates a temporary file with R code and returns the file path.
-// The caller is responsible for removing the temporary file.
-func createTempRScript(content string) (string, func(), error) {
+// RunScript is helpful when running R scripts, they avoid needing to ensure that the contents of the script
+// is fully escaped and quoted.
+func (e *defaultExecutor) RunScript(executable string, args []string, script string, cwd util.AbsolutePath, log logging.Logger) ([]byte, []byte, error) {
+	log.Debug("Running script", "cmd", executable, "args", strings.Join(args, " "), "script", script)
+	// Write script contents to a file, clean it up, and append to args
 	tempFile, err := os.CreateTemp("", "*")
-	if err != nil {
-		return "", nil, fmt.Errorf("failed to create temporary R script: %w", err)
-	}
 
-	// Write the R code to the temporary file
-	if _, err := tempFile.WriteString(content); err != nil {
-		tempFile.Close()
-		os.Remove(tempFile.Name())
-		return "", nil, fmt.Errorf("failed to write to temporary R script: %w", err)
-	}
-	if err := tempFile.Sync(); err != nil {
-		tempFile.Close()
-		os.Remove(tempFile.Name())
-		return "", nil, fmt.Errorf("failed to flush temporary R script to disk: %w", err)
-	}
-
-	// Return the file path and a cleanup function
+	// defer cleaning up
 	cleanup := func() {
 		tempFile.Close()
 		os.Remove(tempFile.Name())
 	}
-	return tempFile.Name(), cleanup, nil
-}
-
-func (e *defaultExecutor) RunScript(executable string, args []string, script string, cwd util.AbsolutePath, log logging.Logger) ([]byte, []byte, error) {
-	log.Debug("Running script", "cmd", executable, "args", strings.Join(args, " "), "script", script)
-	// Write script contents to a file, clean it up, and append to args
-	tempScriptPath, cleanup, scriptErr := createTempRScript(script)
-	if scriptErr != nil {
-		return nil, nil, scriptErr
-	}
 	defer cleanup()
-	args = append(args, "-f", tempScriptPath)
-
-	cmd := exec.Command(executable, args...)
-	cmd.Dir = cwd.String()
-	var stdout bytes.Buffer
-	cmd.Stdout = &stdout
-	stderrBuf := new(bytes.Buffer)
-	cmd.Stderr = stderrBuf
-	err := cmd.Run()
 
 	if err != nil {
-		log.Error("Error running command", "command", executable, "error", err.Error())
-		os.Stderr.Write(stderrBuf.Bytes())
+		return nil, nil, fmt.Errorf("failed to create temporary R script: %w", err)
 	}
-	return stdout.Bytes(), stderrBuf.Bytes(), err
+
+	tempFile.WriteString(script)
+	tempFile.Sync()
+
+	args = append(args, "-f", tempFile.Name())
+	return e.RunCommand(executable, args, cwd, log)
 }
