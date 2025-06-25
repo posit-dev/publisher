eaac925cf5a756ee60b48063bcb1ef557f903e7d
Chris Tierney <chris.tierney@gmail.com>
2025-06-25 18:55:45 -0400

    Always display snowflake connection picker.
    It's confusing when a single option is automatically picked.

--- extensions/vscode/src/multiStepInputs/newCredential.ts
+++ extensions/vscode/src/multiStepInputs/newCredential.ts
@@ -332,33 +332,27 @@ export async function newCredential(
         return;
       }
 
-      let connectionIndex = 0;
-
       // skip if we only have one choice.
-      if (connectionQuickPicks.length > 1) {
-        const pick = await input.showQuickPick({
-          title: state.title,
-          step: thisStepNumber,
-          totalSteps: state.totalSteps,
-          placeholder:
-            "Select the Snowflake connection configuration you want to use to authenticate.",
-          items: connectionQuickPicks,
-          buttons: [],
-          shouldResume: () => Promise.resolve(false),
-          ignoreFocusOut: true,
-        });
+      const pick = await input.showQuickPick({
+        title: state.title,
+        step: thisStepNumber,
+        totalSteps: state.totalSteps,
+        placeholder:
+          "Select the Snowflake connection configuration you want to use to authenticate.",
+        items: connectionQuickPicks,
+        buttons: [],
+        shouldResume: () => Promise.resolve(false),
+        ignoreFocusOut: true,
+      });
 
-        if (!pick || !isQuickPickItemWithIndex(pick)) {
-          return;
-        }
-
-        connectionIndex = pick.index;
+      if (!pick || !isQuickPickItemWithIndex(pick)) {
+        return;
       }
 
       // only one of api key and snowflake connection should be configured
       state.data.apiKey = "";
-      state.data.snowflakeConnection = connections[connectionIndex].name;
-      state.data.url = connections[connectionIndex].serverUrl;
+      state.data.snowflakeConnection = connections[pick.index].name;
+      state.data.url = connections[pick.index].serverUrl;
       state.lastStep = thisStepNumber;
       return (input: MultiStepInput) => inputCredentialName(input, state);
     }
