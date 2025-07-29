22539058909785bf14b25b7788214a531f5b32f1
Matthew Lynch <matthew.lynch@posit.co>
2025-07-29 17:07:46 -0500

    serverType -> productType

--- extensions/vscode/src/multiStepInputs/newDeployment.ts
+++ extensions/vscode/src/multiStepInputs/newDeployment.ts
@@ -2,40 +2,41 @@
 
 import path from "path";
 import {
+  isQuickPickItem,
+  isQuickPickItemWithIndex,
+  isQuickPickItemWithInspectionResult,
   MultiStepInput,
   MultiStepState,
-  QuickPickItemWithInspectionResult,
   QuickPickItemWithIndex,
-  isQuickPickItem,
-  isQuickPickItemWithInspectionResult,
-  isQuickPickItemWithIndex,
+  QuickPickItemWithInspectionResult,
 } from "src/multiStepInputs/multiStepHelper";
 
 import {
+  commands,
   InputBoxValidationSeverity,
   QuickPickItem,
   QuickPickItemKind,
   ThemeIcon,
   Uri,
-  commands,
   window,
   workspace,
 } from "vscode";
 
 import {
-  useApi,
-  Credential,
-  Configuration,
-  PreContentRecord,
-  contentTypeStrings,
-  ConfigurationInspectionResult,
-  EntryPointPath,
   areInspectionResultsSimilarEnough,
+  Configuration,
+  ConfigurationInspectionResult,
   ContentType,
+  contentTypeStrings,
+  Credential,
+  EntryPointPath,
   FileAction,
-  SnowflakeConnection,
-  ServerType,
   PlatformName,
+  PreContentRecord,
+  ProductType,
+  ServerType,
+  SnowflakeConnection,
+  useApi,
 } from "src/api";
 import {
   getPythonInterpreterPath,
@@ -46,7 +47,7 @@ import {
   getSummaryStringFromError,
 } from "src/utils/errors";
 import { isAxiosErrorWithJson } from "src/utils/errorTypes";
-import { newDeploymentName, newConfigFileNameFromTitle } from "src/utils/names";
+import { newConfigFileNameFromTitle, newDeploymentName } from "src/utils/names";
 import { formatURL } from "src/utils/url";
 import { checkSyntaxApiKey } from "src/utils/apiKeys";
 import { DeploymentObjects } from "src/types/shared";
@@ -988,8 +989,10 @@ export async function newDeployment(
       existingNames,
     );
 
-    newDeploymentData.entrypoint.inspectionResult.configuration.serverType =
-      serverType;
+    newDeploymentData.entrypoint.inspectionResult.configuration.productType =
+      serverType === ServerType.CONNECT
+        ? ProductType.CONNECT
+        : ProductType.CONNECT_CLOUD;
 
     configCreateResponse = (
       await api.configurations.createOrUpdate(
