1335aa5c6ad13a29d34c50733556dad067cbcb72
Matthew Lynch <matthew.lynch@posit.co>
2025-08-01 12:44:39 -0500

    formatting

--- internal/accounts/account.go
+++ internal/accounts/account.go
@@ -8,17 +8,17 @@ import (
 // Copyright (C) 2023 by Posit Software, PBC.
 
 type Account struct {
-	ServerType          server_type.ServerType `json:"type"`              // Which type of API this server provides
-	Source              AccountSource          `json:"source"`            // Source of the saved server configuration
-	Name                string                 `json:"name"`              // Nickname
-	URL                 string                 `json:"url"`               // Server URL, e.g. https://connect.example.com/rsc
-	Insecure            bool                   `json:"insecure"`          // Skip https server verification
-	Certificate         string                 `json:"-"`                 // Root CA certificate, if server cert is signed by a private CA
-	AccountName         string                 `json:"account_name"`      // Username, if known
-	ApiKey              string                 `json:"-"`                 // For Connect servers
-	SnowflakeConnection string                 `json:"-"`                 // Snowflake connection name used instead of API Key in SPCS
-	CloudEnvironment    types.CloudEnvironment `json:"cloud_environment"` // Environment for Connect Cloud (production, staging, development)
-	CloudAccountID      string                 `json:"cloud_account_id"`  // Account ID for Connect Cloud
+	ServerType          server_type.ServerType `json:"type"`               // Which type of API this server provides
+	Source              AccountSource          `json:"source"`             // Source of the saved server configuration
+	Name                string                 `json:"name"`               // Nickname
+	URL                 string                 `json:"url"`                // Server URL, e.g. https://connect.example.com/rsc
+	Insecure            bool                   `json:"insecure"`           // Skip https server verification
+	Certificate         string                 `json:"-"`                  // Root CA certificate, if server cert is signed by a private CA
+	AccountName         string                 `json:"account_name"`       // Username, if known
+	ApiKey              string                 `json:"-"`                  // For Connect servers
+	SnowflakeConnection string                 `json:"-"`                  // Snowflake connection name used instead of API Key in SPCS
+	CloudEnvironment    types.CloudEnvironment `json:"cloud_environment"`  // Environment for Connect Cloud (production, staging, development)
+	CloudAccountID      string                 `json:"cloud_account_id"`   // Account ID for Connect Cloud
 	CloudAccountName    string                 `json:"cloud_account_name"` // Account name for Connect Cloud
 }
 
