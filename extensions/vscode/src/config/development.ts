a897c191dca1deaa938f86d90148d4b77e807d4c
rodriin <ingrid.rodriguez@gmail.com>
2025-08-04 20:07:02 -0400

    Lint fix mising copyright on new TS files

--- extensions/vscode/src/config/development.ts
+++ extensions/vscode/src/config/development.ts
@@ -1,3 +1,5 @@
+// Copyright (C) 2025 by Posit Software, PBC.
+
 export const development = {
   cloudURL: "https://login.staging.posit.cloud",
   connectCloudURL: "https://dev.connect.posit.cloud",
--- extensions/vscode/src/config/staging.ts
+++ extensions/vscode/src/config/staging.ts
@@ -1,3 +1,5 @@
+// Copyright (C) 2025 by Posit Software, PBC.
+
 export const staging = {
   cloudURL: "https://login.staging.posit.cloud",
   connectCloudURL: "https://staging.connect.posit.cloud",
