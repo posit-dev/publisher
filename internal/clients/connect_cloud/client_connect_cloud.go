368679a49f1854e3ed73ca4112ba6b2810d1b8fe
Matthew Lynch <matthew.lynch@posit.co>
2025-08-05 08:44:59 -0500

    Add clients/methods for calling Connect Cloud APIs (#2766)

--- internal/clients/connect_cloud/client_connect_cloud.go
+++ internal/clients/connect_cloud/client_connect_cloud.go
@@ -3,12 +3,13 @@ package connect_cloud
 // Copyright (C) 2025 by Posit Software, PBC.
 
 import (
+	"fmt"
 	"time"
 
-	"github.com/posit-dev/publisher/internal/types"
-
 	"github.com/posit-dev/publisher/internal/clients/http_client"
+	clienttypes "github.com/posit-dev/publisher/internal/clients/types"
 	"github.com/posit-dev/publisher/internal/logging"
+	"github.com/posit-dev/publisher/internal/types"
 )
 
 const baseURLDevelopment = "https://api.dev.connect.posit.cloud"
@@ -61,4 +62,51 @@ func (c ConnectCloudClient) GetAccounts() (*AccountListResponse, error) {
 		return nil, err
 	}
 	return &into, nil
+}
+
+func (c ConnectCloudClient) CreateContent(request *clienttypes.CreateContentRequest) (*clienttypes.ContentResponse, error) {
+	into := clienttypes.ContentResponse{}
+	err := c.client.Post("/v1/contents", request, &into, c.log)
+	if err != nil {
+		return nil, err
+	}
+	return &into, nil
+}
+
+func (c ConnectCloudClient) UpdateContent(request *clienttypes.UpdateContentRequest) (*clienttypes.ContentResponse, error) {
+	into := clienttypes.ContentResponse{}
+	url := fmt.Sprintf("/v1/contents/%s?new_bundle=true", request.ContentID)
+	err := c.client.Patch(url, &request.ContentRequestBase, &into, c.log)
+	if err != nil {
+		return nil, err
+	}
+	return &into, nil
+}
+
+func (c ConnectCloudClient) GetRevision(revisionID string) (*clienttypes.Revision, error) {
+	into := clienttypes.Revision{}
+	url := fmt.Sprintf("/v1/revisions/%s", revisionID)
+	err := c.client.Get(url, &into, c.log)
+	if err != nil {
+		return nil, err
+	}
+	return &into, nil
+}
+
+func (c ConnectCloudClient) GetAuthorization(request *clienttypes.AuthorizationRequest) (*clienttypes.AuthorizationResponse, error) {
+	into := clienttypes.AuthorizationResponse{}
+	err := c.client.Post("/v1/authorization", request, &into, c.log)
+	if err != nil {
+		return nil, err
+	}
+	return &into, nil
+}
+
+func (c ConnectCloudClient) PublishContent(contentID string) error {
+	url := fmt.Sprintf("/v1/content/%s/publish", contentID)
+	err := c.client.Post(url, nil, nil, c.log)
+	if err != nil {
+		return err
+	}
+	return nil
 }
