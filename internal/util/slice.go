8c43fa5a27e1020e2fc873440bc4fa54c1029da0
Matthew Lynch <matthew.lynch@posit.co>
2025-08-18 14:10:15 -0500

    remove unused RemoveDuplicates

--- internal/util/slice.go
+++ internal/util/slice.go
@@ -2,19 +2,6 @@ package util
 
 // Copyright (C) 2023 by Posit Software, PBC.
 
-func RemoveDuplicates[T comparable](list []T) []T {
-	seen := map[T]bool{}
-	out := []T{}
-	for _, item := range list {
-		_, ok := seen[item]
-		if !ok {
-			seen[item] = true
-			out = append(out, item)
-		}
-	}
-	return out
-}
-
 func Map[T any](fn func(T) T, list []T) []T {
 	out := make([]T, len(list))
 	for i, item := range list {
