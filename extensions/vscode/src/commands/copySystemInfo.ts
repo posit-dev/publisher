cee82d03b0b557b881b0d773fd0d8c999222220b
Jordan Jensen <jordan.jensen@rstudio.com>
2025-08-19 10:32:23 -0700

    Simplify to use env.appName

--- extensions/vscode/src/commands/copySystemInfo.ts
+++ extensions/vscode/src/commands/copySystemInfo.ts
@@ -10,23 +10,12 @@ export async function copySystemInfoCommand(
 ): Promise<void> {
   const extensionVersion = context.extension.packageJSON.version || "unknown";
 
-  let vscodeProductName: string;
-  if (env.appName.includes("Positron")) {
-    vscodeProductName = "Positron";
-  } else if (
-    env.appName.includes("Visual Studio Code") ||
-    env.appName === "Code"
-  ) {
-    vscodeProductName = "VS Code";
-  } else {
-    vscodeProductName = env.appName;
-  }
   const systemInfo = [
     "## System Information",
     "",
     `- **Posit Publisher Version**: ${extensionVersion}`,
     `- **Platform**: ${process.platform} ${process.arch}`,
-    `- **IDE**: ${vscodeProductName} ${version}`,
+    `- **IDE**: ${env.appName} ${version}`,
   ].join("\n");
 
   await env.clipboard.writeText(systemInfo);
