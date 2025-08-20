51bb0b6e19564aefd03153d10c4ce5e3e4b64e0d
rodriin <ingrid.rodriguez@gmail.com>
2025-08-20 07:40:37 -0400

    Updating the polling interval default to 1 second

--- extensions/vscode/src/multiStepInputs/multiStepHelper.ts
+++ extensions/vscode/src/multiStepInputs/multiStepHelper.ts
@@ -406,8 +406,8 @@ export class MultiStepInput {
         // eslint-disable-next-line no-async-promise-executor
         async (resolve, reject) => {
           let abortPolling = false;
-          // default the polling interval to 5 seconds
-          pollingInterval ||= 5000;
+          // default the polling interval to 1 second
+          pollingInterval ||= 1000;
           const input = window.createInputBox();
           input.title = title;
           input.step = step;
@@ -490,8 +490,8 @@ export class MultiStepInput {
             }
           };
 
-          // start polling (poll every 5 seconds at most 120 times === 10 minutes max time)
-          return pollApiFunction(pollingInterval, 120)
+          // start polling (poll every 1 second at most 600 times === 10 minutes max time)
+          return pollApiFunction(pollingInterval, 600)
             .then(resolve)
             .catch(reject);
         },
