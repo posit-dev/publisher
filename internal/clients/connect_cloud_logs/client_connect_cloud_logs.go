187170e7e8d5b9d8672366a5b2bf516a97a133fa
Matthew Lynch <matthew.lynch@posit.co>
2025-08-25 14:26:21 -0500

    remove comments

--- internal/clients/connect_cloud_logs/client_connect_cloud_logs.go
+++ internal/clients/connect_cloud_logs/client_connect_cloud_logs.go
@@ -78,7 +78,6 @@ func (c ConnectCloudLogsClient) WatchLogs(ctx context.Context, logLogger logging
 				}
 
 				for _, line := range logsResponse.Data {
-					//if line.Type == LogTypeBuild {
 					switch line.Level {
 					case LogLevelDebug:
 						logLogger.Debug(line.Message)
@@ -87,7 +86,6 @@ func (c ConnectCloudLogsClient) WatchLogs(ctx context.Context, logLogger logging
 					default:
 						logLogger.Info(line.Message)
 					}
-					//}
 				}
 			}
 		}
