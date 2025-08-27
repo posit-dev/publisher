dcc1bebe63f8ce33977228f40d2ac9ef6a4c8b96
Byonca Honaker <byonca.honaker@posit.co>
2025-08-27 14:29:05 -0500

    feat: celebratory message when credential token is added

--- extensions/vscode/src/multiStepInputs/newCredential.ts
+++ extensions/vscode/src/multiStepInputs/newCredential.ts
@@ -6,6 +6,7 @@ import {
   MultiStepInput,
   MultiStepState,
 } from "./multiStepHelper";
+import { window } from "vscode";
 import { Credential, ServerType, ProductName } from "src/api";
 import { extensionSettings } from "src/extension";
 import { platformList } from "src/multiStepInputs/common";
@@ -167,6 +168,11 @@ export async function newCredential(
     // navigating backwards and then forward in the multi-stepper steps
     throw new AbortError();
   }
+
+  window.showInformationMessage(
+    // To prevent a large refactor for the flow, we added the as Credential declaration
+    `Successfully added ${(credential as Credential).name} 🎉`,
+  );
 
   return credential;
 }
