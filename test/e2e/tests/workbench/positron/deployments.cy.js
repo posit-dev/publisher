5499e7cd088b2695080735a1712c0d5d225df0da
Christopher Graham <christopher.graham@posit.co>
2025-09-03 16:28:47 -0400

    Get Workbench tests working with latest UI changes

--- test/e2e/tests/workbench/positron/deployments.cy.js
+++ test/e2e/tests/workbench/positron/deployments.cy.js
@@ -3,44 +3,47 @@
 const WORKBENCH_BASE_URL = Cypress.env("WORKBENCH_URL");
 
 describe("Workbench > Positron", { baseUrl: WORKBENCH_BASE_URL }, () => {
-  // Each test must set this var to enable project-specific cleanup in afterEach
-  let projectDir;
+  context("Connect", () => {
+    // Each test must set this var to enable project-specific cleanup in afterEach
+    let projectDir;
 
-  beforeEach(() => {
-    cy.resetConnect();
-    cy.setAdminCredentials();
+    beforeEach(() => {
+      cy.resetConnect();
+      cy.setAdminCredentials();
 
-    cy.cleanupAndRestartWorkbench();
-    cy.visitAndLoginToWorkbench();
-  });
+      cy.cleanupAndRestartWorkbench();
+      cy.visitAndLoginToWorkbench();
+    });
 
-  afterEach(() => {
-    cy.cleanupWorkbenchData(projectDir);
-  });
+    afterEach(() => {
+      cy.cleanupWorkbenchData(projectDir);
+    });
 
-  it("Static Content Deployment", () => {
-    projectDir = "static";
+    it("Static Content Deployment", () => {
+      projectDir = "static";
 
-    cy.startWorkbenchPositronPythonProject(projectDir);
+      cy.startWorkbenchPositronPythonProject(projectDir);
 
-    // Publish the content
-    cy.createPositronDeployment(
-      projectDir,
-      "index.html",
-      "static",
-      (tomlFiles) => {
-        const config = tomlFiles.config.contents;
-        expect(config.title).to.equal(projectDir);
-        expect(config.type).to.equal("html");
-        expect(config.entrypoint).to.equal("index.html");
-        expect(config.files[0]).to.equal("/index.html");
-        expect(config.files[1]).to.equal(
-          `/.posit/publish/${tomlFiles.config.name}`,
-        );
-        expect(config.files[2]).to.equal(
-          `/.posit/publish/deployments/${tomlFiles.contentRecord.name}`,
-        );
-      },
-    ).deployCurrentlySelected();
+      // Publish the content
+      cy.createPositronDeployment(
+        projectDir,
+        "index.html",
+        "static",
+        (tomlFiles) => {
+          const config = tomlFiles.config.contents;
+          expect(config.title).to.equal(projectDir);
+          expect(config.type).to.equal("html");
+          expect(config.entrypoint).to.equal("index.html");
+          expect(config.files[0]).to.equal("/index.html");
+          expect(config.files[1]).to.equal(
+            `/.posit/publish/${tomlFiles.config.name}`,
+          );
+          expect(config.files[2]).to.equal(
+            `/.posit/publish/deployments/${tomlFiles.contentRecord.name}`,
+          );
+        },
+        "Connect",
+      ).deployCurrentlySelected();
+    });
   });
 });
