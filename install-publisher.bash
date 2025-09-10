54b832c1912a39ae1f20817e6601367b47074b46
Chris Graham <87026636+cgraham-rs@users.noreply.github.com>
2025-09-10 13:15:53 -0400

    Prep for 1.21.0 pre-release (#3059)

--- install-publisher.bash
+++ install-publisher.bash
@@ -148,7 +148,7 @@ esac
 
 # version override, swap out latest with the latest and greatest
 if [[ $VERSION_TYPE == "release" && $VERSION == "latest" ]]; then
-  VERSION="1.20.0"
+  VERSION="1.21.0"
 fi
 
 # Variables
