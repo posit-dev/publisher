536ee273203fea0090d3c5aacfe0f13877cec2a5
Jordan Jensen <jordan.jensen@rstudio.com>
2025-11-14 12:13:56 -0800

    Add import alias to vitest config

--- extensions/vscode/vitest.config.ts
+++ extensions/vscode/vitest.config.ts
@@ -1,9 +1,15 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
+import path from "path";
 
 // Configuration to run unit tests under the ./src dir
 // Excluding vscode extension tests suite
 export default defineConfig({
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     include: ["./src/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
     exclude: ["./src/test/**"],
